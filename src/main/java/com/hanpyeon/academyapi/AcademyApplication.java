8ac9640f84e64087448350de716242b2940e4f59
huhdy32 <mong3268@gmail.com>
2024-01-21 00:08:01 +0900

    init: 프로젝트 이름 변경

--- src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
+++ src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademyapiApplication {
+public class AcademyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AcademyapiApplication.class, args);
+        SpringApplication.run(AcademyApplication.class, args);
     }
 
 }
