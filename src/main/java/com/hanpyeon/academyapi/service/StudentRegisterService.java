fcef99efbeb648d3e9a37b1f34e1b7abab6796d6
huhdy32 <mong3268@gmail.com>
2024-01-21 21:12:55 +0900

    #4 feat: 학생등록 서비스객체 구현

---
+++ src/main/java/com/hanpyeon/academyapi/service/StudentRegisterService.java
@@ -0,0 +1,36 @@
+package com.hanpyeon.academyapi.service;
+
+import com.hanpyeon.academyapi.dto.StudentRegisterRequestDto;
+import com.hanpyeon.academyapi.entity.Member;
+import com.hanpyeon.academyapi.exceptions.AlreadyRegisteredException;
+import com.hanpyeon.academyapi.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentRegisterService {
+    private final Logger logger = LoggerFactory.getLogger("STUDENT_REGISTER_SERVICE");
+    private final UserRepository repository;
+    public StudentRegisterService(final UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public void registerUser(final StudentRegisterRequestDto registerDto) {
+        if (repository.findMemberByPhoneNumber(registerDto.studentPhoneNumber()).isPresent()) {
+            logger.debug("이미 등록된 사용자입니다.");
+            throw new AlreadyRegisteredException();
+        }
+        repository.save(createMemberEntity(registerDto));
+        logger.info(String.valueOf(repository.findMemberByPhoneNumber(registerDto.studentPhoneNumber())));
+    }
+    private Member createMemberEntity(final StudentRegisterRequestDto registerDto) {
+        return Member.builder()
+                .userName(registerDto.studentName())
+                .grade(registerDto.studentGrade())
+                .phoneNumber(registerDto.studentPhoneNumber())
+                .build();
+    }
+}
