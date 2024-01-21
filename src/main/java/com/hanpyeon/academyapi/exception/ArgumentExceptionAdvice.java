4deab11bd7f108111402b3e5bd66d99336dbaa4b
huhdy32 <mong3268@gmail.com>
2024-01-22 08:53:11 +0900

    #4 feat: 예외 처리 ExceptionHandler 추가

--- src/main/java/com/hanpyeon/academyapi/exception/ArgumentExceptionAdvice.java
+++ src/main/java/com/hanpyeon/academyapi/exception/ArgumentExceptionAdvice.java
@@ -1,12 +1,15 @@
 package com.hanpyeon.academyapi.exception;
 
 import com.hanpyeon.academyapi.dto.ExceptionResponseBody;
+import com.hanpyeon.academyapi.exceptions.AlreadyRegisteredException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -18,4 +21,12 @@ public class ArgumentExceptionAdvice {
                 .collect(Collectors.toList());
         return ResponseEntity.badRequest().body(new ExceptionResponseBody("-001", errors));
     }
+
+    @ExceptionHandler(AlreadyRegisteredException.class)
+    public ResponseEntity<?> responseProperCode1(AlreadyRegisteredException exception) {
+        Map<String, String> body = new HashMap<>();
+        body.put("errorCode", "-002");
+        return ResponseEntity.badRequest().body(new ExceptionResponseBody("-002", List.of(exception.getMessage())));
+    }
+
 }
