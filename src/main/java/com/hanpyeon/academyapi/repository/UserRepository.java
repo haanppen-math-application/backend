9bcd9d46832b0c306ed1b443546c5d7900afb4fb
huhdy32 <mong3268@gmail.com>
2024-01-21 21:02:35 +0900

    #4 feat: 전화번호로 학생 찾는 로직 추가

--- src/main/java/com/hanpyeon/academyapi/repository/UserRepository.java
+++ src/main/java/com/hanpyeon/academyapi/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.hanpyeon.academyapi.repository;
 
-import com.hanpyeon.academyapi.entity.User;
+import com.hanpyeon.academyapi.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+
+@Repository
+public interface UserRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByPhoneNumber(String phoneNumber);
 }
