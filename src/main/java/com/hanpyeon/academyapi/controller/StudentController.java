1ebe62f95e904b921684e75d4cb86e8a56dccb6a
huhdy32 <mong3268@gmail.com>
2024-01-21 21:11:38 +0900

    #4 feat: 학생등록 기능을 위한 서비스 빈 주입

--- src/main/java/com/hanpyeon/academyapi/controller/StudentController.java
+++ src/main/java/com/hanpyeon/academyapi/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.hanpyeon.academyapi.controller;
 
 import com.hanpyeon.academyapi.dto.StudentRegisterRequestDto;
+import com.hanpyeon.academyapi.service.StudentRegisterService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     private final Logger LOGGER = LoggerFactory.getLogger("등록 테스트");
 
+    StudentRegisterService registerService;
 
+    public StudentController(StudentRegisterService registerService) {
+        this.registerService = registerService;
+    }
 
     @PostMapping("")
-    public ResponseEntity<?> registerStudent(@Valid @RequestBody StudentRegisterRequestDto studentRegisterRequestDto){
-        LOGGER.info(studentRegisterRequestDto.toString());
-        return ResponseEntity.created(null).build();
+    public ResponseEntity<?> registerStudent(@Valid @RequestBody StudentRegisterRequestDto studentRegisterRequestDto) {
+        registerService.registerUser(studentRegisterRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
