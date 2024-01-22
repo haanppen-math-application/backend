3f55cd33819b213ab1e8a95cfabab633c3eccc3c
huhdy32 <mong3268@gmail.com>
2024-01-22 21:50:55 +0900

    #6 style: 빈칸 제거

--- src/main/java/com/hanpyeon/academyapi/exception/ArgumentExceptionAdvice.java
+++ src/main/java/com/hanpyeon/academyapi/exception/ArgumentExceptionAdvice.java
@@ -21,12 +21,10 @@ public class ArgumentExceptionAdvice {
                 .collect(Collectors.toList());
         return ResponseEntity.badRequest().body(new ExceptionResponseBody("-001", errors));
     }
-
     @ExceptionHandler(AlreadyRegisteredException.class)
     public ResponseEntity<?> responseProperCode1(AlreadyRegisteredException exception) {
         Map<String, String> body = new HashMap<>();
         body.put("errorCode", "-002");
         return ResponseEntity.badRequest().body(new ExceptionResponseBody("-002", List.of(exception.getMessage())));
     }
-
 }
