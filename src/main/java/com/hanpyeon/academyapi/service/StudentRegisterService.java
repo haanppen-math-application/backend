83b5218f20600c967c354a32a24afd9cf72994af
huhdy32 <mong3268@gmail.com>
2024-01-22 13:34:41 +0900

    #6 feat: 비밀번호 여부에 따라 사용자등록 구현

--- src/main/java/com/hanpyeon/academyapi/service/StudentRegisterService.java
+++ src/main/java/com/hanpyeon/academyapi/service/StudentRegisterService.java
@@ -7,30 +7,42 @@ import com.hanpyeon.academyapi.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.function.Function;
 
 @Service
 public class StudentRegisterService {
     private final Logger logger = LoggerFactory.getLogger("STUDENT_REGISTER_SERVICE");
     private final UserRepository repository;
-    public StudentRegisterService(final UserRepository repository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public StudentRegisterService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
-    public void registerUser(final StudentRegisterRequestDto registerDto) {
-        if (repository.findMemberByPhoneNumber(registerDto.studentPhoneNumber()).isPresent()) {
-            logger.debug("이미 등록된 학생");
-            throw new AlreadyRegisteredException("이미 등록딘 학생입니다.");
+    public void registerMember(@Validated final StudentRegisterRequestDto requestDto) {
+        if (repository.findMemberByPhoneNumber(requestDto.studentPhoneNumber()).isPresent()) {
+            logger.debug("이미 등록된 사용자(전화번호) 입니다.");
+            throw new AlreadyRegisteredException("이미 등록된 전화번호 입니다.");
         }
-        repository.save(createMemberEntity(registerDto));
-        logger.info(String.valueOf(repository.findMemberByPhoneNumber(registerDto.studentPhoneNumber())));
+        Member member;
+        if (!requestDto.password().isBlank()) {
+            member = processPassword(requestDto, (str) -> passwordEncoder.encode(str));
+        }
+        member = processPassword(requestDto, (str) -> str);
+        repository.save(member);
     }
-    private Member createMemberEntity(final StudentRegisterRequestDto registerDto) {
+    private Member processPassword(final StudentRegisterRequestDto requestDto, Function<String, String> function) {
         return Member.builder()
-                .userName(registerDto.studentName())
-                .grade(registerDto.studentGrade())
-                .phoneNumber(registerDto.studentPhoneNumber())
+                .memberName(requestDto.studentName())
+                .password(function.apply(requestDto.password()))
+                .grade(requestDto.studentGrade())
+                .phoneNumber(requestDto.studentPhoneNumber())
                 .build();
     }
 }
