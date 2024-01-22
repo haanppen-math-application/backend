ba38bb0ecdd19698e365ebd35ee3b4dd9c1332c0
huhdy32 <mong3268@gmail.com>
2024-01-22 13:41:14 +0900

    #6 refactor: 메소드 분리

--- src/main/java/com/hanpyeon/academyapi/service/StudentRegisterService.java
+++ src/main/java/com/hanpyeon/academyapi/service/StudentRegisterService.java
@@ -26,19 +26,24 @@ public class StudentRegisterService {
 
     @Transactional
     public void registerMember(@Validated final StudentRegisterRequestDto requestDto) {
+        validateMember(requestDto);
+        Member member = createMember(requestDto, getProperPasswordProcess(requestDto));
+        repository.save(member);
+    }
+    private Function<String, String> getProperPasswordProcess(StudentRegisterRequestDto requestDto) {
+        if (!requestDto.password().isBlank()) {
+            return (str) -> passwordEncoder.encode(str);
+        }else {
+            return (str) -> str;
+        }
+    }
+    private void validateMember(final StudentRegisterRequestDto requestDto) {
         if (repository.findMemberByPhoneNumber(requestDto.studentPhoneNumber()).isPresent()) {
             logger.debug("이미 등록된 사용자(전화번호) 입니다.");
             throw new AlreadyRegisteredException("이미 등록된 전화번호 입니다.");
         }
-        Member member;
-        if (!requestDto.password().isBlank()) {
-            member = processPassword(requestDto, (str) -> passwordEncoder.encode(str));
-        }else {
-            member = processPassword(requestDto, (str) -> str);
-        }
-        repository.save(member);
     }
-    private Member processPassword(final StudentRegisterRequestDto requestDto, Function<String, String> function) {
+    private Member createMember(final StudentRegisterRequestDto requestDto, Function<String, String> function) {
         return Member.builder()
                 .memberName(requestDto.studentName())
                 .password(function.apply(requestDto.password()))
