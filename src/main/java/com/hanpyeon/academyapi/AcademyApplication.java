28af10cb3f2ac41763b07eb684fc8aebea85c4a0
huhdy32 <mong3268@gmail.com>
2024-01-22 16:24:13 +0900

    #6 refactor: 불필요한 import 제거

--- src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
+++ src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
@@ -2,8 +2,6 @@ package com.hanpyeon.academyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AcademyApplication {
--- src/test/java/com/hanpyeon/academyapi/service/PasswordHandlerTest.java
+++ src/test/java/com/hanpyeon/academyapi/service/PasswordHandlerTest.java
@@ -2,7 +2,6 @@ package com.hanpyeon.academyapi.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,8 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.stream.Stream;
-
-import static org.mockito.ArgumentMatchers.eq;
 
 @ExtendWith(MockitoExtension.class)
 public class PasswordHandlerTest {
