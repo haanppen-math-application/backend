9f07370513aa9a253024f0da1624bde37c2b4cae
huhdy32 <mong3268@gmail.com>
2024-01-23 22:03:55 +0900

    #4 feat(RegisterRequestDto): 검증로직 추가

--- src/main/java/com/hanpyeon/academyapi/dto/RegisterRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/dto/RegisterRequestDto.java
@@ -1,14 +1,20 @@
 package com.hanpyeon.academyapi.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Range;
 
 @Builder
 public record RegisterRequestDto(
         @NotBlank
         String userName,
         @NotBlank
+        @Pattern(regexp = "^[0-9]+$")
         String userPhoneNumber,
+        @Range(min = 0, max = 11)
+        @Nullable
         Integer grade,
         String password
 ) {
