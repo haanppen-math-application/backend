f7cfbb97b6b7b1ec64b25268b5be42bf159bc063
huhdy32 <mong3268@gmail.com>
2024-01-23 21:52:07 +0900

    #4 style(StudentRegisterRequestDto): 필드 순서 정렬

--- src/main/java/com/hanpyeon/academyapi/dto/StudentRegisterRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/dto/StudentRegisterRequestDto.java
@@ -3,12 +3,19 @@ package com.hanpyeon.academyapi.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import org.hibernate.validator.constraints.Range;
 
+@Builder
 public record StudentRegisterRequestDto(
-        @NotBlank String studentName,
-        @NotNull @Range(min = 0, max = 11) Integer studentGrade,
-        @NotBlank @Pattern(regexp = "^[0-9]+$") String studentPhoneNumber,
-        String password
+        @NotBlank
+        String studentName,
+        @NotBlank
+        @Pattern(regexp = "^[0-9]+$")
+        String studentPhoneNumber,
+        @Range(min = 0, max = 11)
+        @NotNull
+        Integer studentGrade,
+        String studentPassword
 ) {
 }
