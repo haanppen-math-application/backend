b1f7c7795039dd74a641dae48f9a301cfd279be0
huhdy32 <mong3268@gmail.com>
2024-01-25 11:39:16 +0900

    #9 refactor(RegisterRequestDto): grade 필드 @NotNull 제거

--- src/main/java/com/hanpyeon/academyapi/dto/RegisterRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/dto/RegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.hanpyeon.academyapi.dto;
 
+import com.hanpyeon.academyapi.security.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -7,8 +8,9 @@ import org.hibernate.validator.constraints.Range;
 
 public record RegisterRequestDto(
         @NotBlank String name,
-        @NotNull @Range(min = 0, max = 11) Integer grade,
+        @Range(min = 0, max = 11) Integer grade,
         @NotBlank @Pattern(regexp = "^[0-9]+$") String phoneNumber,
+        @NotBlank Role role,
         String password
 ) {
 }
