aa49b5da6d46988868f6fb4d56627b14da4d1051
huhdy32 <mong3268@gmail.com>
2024-01-25 11:24:18 +0900

    #9 refactor(RegisterService): 등록 요청 유형별 검사 구현

--- src/main/java/com/hanpyeon/academyapi/service/RegisterService.java
+++ src/main/java/com/hanpyeon/academyapi/service/RegisterService.java
@@ -1,50 +1,29 @@
 package com.hanpyeon.academyapi.service;
 
-import com.hanpyeon.academyapi.dto.RegisterRequestDto;
-import com.hanpyeon.academyapi.entity.Member;
-import com.hanpyeon.academyapi.exceptions.AlreadyRegisteredException;
-import com.hanpyeon.academyapi.repository.UserRepository;
-import jakarta.transaction.Transactional;
+import com.hanpyeon.academyapi.dto.RegisterMemberDto;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class RegisterService {
-    private final Logger logger = LoggerFactory.getLogger("STUDENT_REGISTER_SERVICE");
-    private final UserRepository repository;
-    private final PasswordHandler passwordHandler;
+    private final static Logger LOGGER = LoggerFactory.getLogger("RegisterRoleChecker");
+    private final RegisterServiceProvider registerServiceProvider;
+    private final List<MemberVerification> verifications;
 
-    public RegisterService(UserRepository repository, PasswordHandler passwordHandler) {
-        this.repository = repository;
-        this.passwordHandler = passwordHandler;
+    public void register(RegisterMemberDto memberDto) {
+        verify(memberDto);
+        registerServiceProvider.registerMember(memberDto);
     }
-
-    @Transactional
-    public void registerMember(@Validated final RegisterRequestDto requestDto) {
-        validateRegisterRequest(requestDto);
-
-        String encodedPassword = passwordHandler.getEncodedPassword(requestDto.password());
-        Member member = createMember(requestDto, encodedPassword);
-
-        repository.save(member);
-        logger.info(member.toString());
-    }
-
-    private void validateRegisterRequest(final RegisterRequestDto requestDto) {
-        if (repository.findMemberByPhoneNumber(requestDto.phoneNumber()).isPresent()) {
-            logger.debug("이미 등록된 사용자(전화번호) 입니다.");
-            throw new AlreadyRegisteredException("이미 등록된 전화번호 입니다.");
-        }
-    }
-
-    private Member createMember(final RegisterRequestDto requestDto, String encodedPassword) {
-        return Member.builder()
-                .memberName(requestDto.name())
-                .password(encodedPassword)
-                .grade(requestDto.grade())
-                .phoneNumber(requestDto.phoneNumber())
-                .build();
+    private void verify(RegisterMemberDto memberDto) {
+        verifications.stream()
+                .filter(memberVerification -> memberVerification.supports(memberDto))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new)
+                .checkFields(memberDto);
     }
 }
