8d46e6b1f8bf121e699a37fe815c370908a1b8a1
huhdy32 <mong3268@gmail.com>
2024-01-26 15:07:00 +0900

    #10 fix(RegisterMapper): 컴포넌트로 추가

--- src/main/java/com/hanpyeon/academyapi/mapper/RegisterMapper.java
+++ src/main/java/com/hanpyeon/academyapi/mapper/RegisterMapper.java
@@ -3,9 +3,11 @@ package com.hanpyeon.academyapi.mapper;
 import com.hanpyeon.academyapi.dto.RegisterMemberDto;
 import com.hanpyeon.academyapi.dto.RegisterRequestDto;
 import com.hanpyeon.academyapi.entity.Member;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class RegisterMapper {
     public RegisterMemberDto createRegisterMemberDto(final RegisterRequestDto requestDto, final LocalDateTime dateTime) {
         return RegisterMemberDto.builder()
