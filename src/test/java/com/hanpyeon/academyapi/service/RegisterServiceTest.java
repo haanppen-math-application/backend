bdb395aa20feeee520ffc8f9aff91dbab89af71a
huhdy32 <mong3268@gmail.com>
2024-01-26 19:53:39 +0900

    #16 test(RegisterServiceTest): 현재시간 삽입 테스트

--- src/test/java/com/hanpyeon/academyapi/service/RegisterServiceTest.java
+++ src/test/java/com/hanpyeon/academyapi/service/RegisterServiceTest.java
@@ -1,27 +1,26 @@
 package com.hanpyeon.academyapi.service;
 
 import com.hanpyeon.academyapi.dto.RegisterMemberDto;
-import com.hanpyeon.academyapi.entity.Member;
+import com.hanpyeon.academyapi.dto.RegisterMemberTotalDto;
+import com.hanpyeon.academyapi.mapper.RegisterMapper;
 import com.hanpyeon.academyapi.security.Role;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.ArgumentMatchers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
 class RegisterServiceTest {
     @Mock
     MemberVerification strategy1;
@@ -31,39 +30,55 @@ class RegisterServiceTest {
     MemberVerification strategy3;
     @Mock
     RegisterServiceProvider serviceProvider;
+    @Mock
+    TimeProvider timeProvider;
+    RegisterMapper registerMapper = new RegisterMapper();
     RegisterService registerService;
+
     @BeforeEach
     void setUp() {
-        strategy1 = Mockito.mock(MemberVerification.class);
-        strategy2 = Mockito.mock(MemberVerification.class);
-        strategy3 = Mockito.mock(MemberVerification.class);
-        serviceProvider = Mockito.mock(RegisterServiceProvider.class);
-        this.registerService = new RegisterService(serviceProvider, new ArrayList<>(List.of(strategy1, strategy2, strategy3)));
+        this.registerService = new RegisterService(serviceProvider, List.of(strategy1, strategy2, strategy3), registerMapper, timeProvider);
     }
 
     @ParameterizedTest
     @MethodSource("provideRegisterRequest")
-    void 등록_유형별_테스트_실행 (RegisterMemberDto memberDto) {
+    void 등록_유형별_테스트_실행(RegisterMemberDto memberDto) {
         try {
             registerService.register(memberDto);
-        }catch (IllegalArgumentException e){}
+        } catch (IllegalArgumentException e) {
+        }
 
         Mockito.verify(strategy1).supports(memberDto);
         Mockito.verify(strategy2).supports(memberDto);
         Mockito.verify(strategy3).supports(memberDto);
     }
+
     @ParameterizedTest
     @MethodSource("provideRegisterRequest")
     void 단일_실행_테스트(RegisterMemberDto memberDto) {
-        Mockito.when(strategy1.supports(memberDto)).thenReturn(true);
-        Mockito.when(strategy2.supports(memberDto)).thenReturn(true);
-        Mockito.when(strategy3.supports(memberDto)).thenReturn(true);
+        Mockito.lenient().when(strategy1.supports(memberDto)).thenReturn(true);
+        Mockito.lenient().when(strategy2.supports(memberDto)).thenReturn(true);
+        Mockito.lenient().when(strategy3.supports(memberDto)).thenReturn(true);
 
         registerService.register(memberDto);
 
         Mockito.verify(strategy1).checkFields(memberDto);
         Mockito.verify(strategy2, Mockito.never()).checkFields(memberDto);
         Mockito.verify(strategy3, Mockito.never()).checkFields(memberDto);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRegisterRequest")
+    void 현재_시간_삽입_테스트(RegisterMemberDto memberDto) {
+        LocalDateTime currTime = LocalDateTime.now();
+        Mockito.when(strategy1.supports(memberDto)).thenReturn(true);
+        Mockito.when(timeProvider.getCurrTime()).thenReturn(currTime);
+        ArgumentCaptor<RegisterMemberTotalDto> argumentCaptor = ArgumentCaptor.forClass(RegisterMemberTotalDto.class);
+
+        registerService.register(memberDto);
+        Mockito.verify(serviceProvider).registerMember(argumentCaptor.capture());
+
+        assertEquals(argumentCaptor.getValue().registerDate(), currTime);
 
     }
 
@@ -74,7 +89,6 @@ class RegisterServiceTest {
                 .phoneNumber(phoneNumber)
                 .role(role)
                 .password(password)
-                .registerDate(LocalDateTime.now())
                 .build();
     }
 
@@ -83,7 +97,7 @@ class RegisterServiceTest {
         return Stream.of(
                 Arguments.of(createMemberDto("Heejong", 10, Role.ROLE_STUDENT, "01099182281", "000")),
                 Arguments.of(createMemberDto("Hee12", 11, Role.ROLE_TEACHER, "010991822281", "121")),
-                Arguments.of(createMemberDto("Heejong", 10, Role.ROLE_STUDENT,"02109931822813", "124")),
+                Arguments.of(createMemberDto("Heejong", 10, Role.ROLE_STUDENT, "02109931822813", "124")),
                 Arguments.of(createMemberDto("Heejong", 10, Role.ROLE_TEACHER, "120109918122281", ""))
         );
     }
