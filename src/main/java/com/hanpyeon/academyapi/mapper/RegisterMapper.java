12b04dc6d76dc54844234779bc78775c34de716f
huhdy32 <mong3268@gmail.com>
2024-01-26 15:05:23 +0900

    #10 refactor(RegisterMapper): 매퍼 분리

---
+++ src/main/java/com/hanpyeon/academyapi/mapper/RegisterMapper.java
@@ -0,0 +1,29 @@
+package com.hanpyeon.academyapi.mapper;
+
+import com.hanpyeon.academyapi.dto.RegisterMemberDto;
+import com.hanpyeon.academyapi.dto.RegisterRequestDto;
+import com.hanpyeon.academyapi.entity.Member;
+
+import java.time.LocalDateTime;
+
+public class RegisterMapper {
+    public RegisterMemberDto createRegisterMemberDto(final RegisterRequestDto requestDto, final LocalDateTime dateTime) {
+        return RegisterMemberDto.builder()
+                .name(requestDto.name())
+                .phoneNumber(requestDto.phoneNumber())
+                .grade(requestDto.grade())
+                .password(requestDto.password())
+                .role(requestDto.role())
+                .registerDate(dateTime)
+                .build();
+    }
+    public Member createMemberEntity(final RegisterMemberDto registerMemberDto, final String encodedPassword) {
+        return Member.builder()
+                .memberName(registerMemberDto.name())
+                .phoneNumber(registerMemberDto.phoneNumber())
+                .grade(registerMemberDto.grade())
+                .password(encodedPassword)
+                .userRole(registerMemberDto.role())
+                .build();
+    }
+}
