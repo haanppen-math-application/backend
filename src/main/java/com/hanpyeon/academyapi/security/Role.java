55219b7720c9368655ef6c9df7938f798eba8f26
huhdy32 <mong3268@gmail.com>
2024-01-29 12:04:09 +0900

    #20 refactor(Role): 내부 로직과 외부 요청간의 결합도 완화 로직

--- src/main/java/com/hanpyeon/academyapi/security/Role.java
+++ src/main/java/com/hanpyeon/academyapi/security/Role.java
@@ -1,7 +1,25 @@
 package com.hanpyeon.academyapi.security;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+
 public enum Role {
-    ROLE_STUDENT,
-    ROLE_TEACHER,
-    ROLE_MANAGER
+    ROLE_STUDENT("student"),
+    ROLE_TEACHER("teacher"),
+    ROLE_MANAGER("manager");
+
+    Role(String identifier) {
+        this.identifier = identifier;
+    }
+
+    private final String identifier;
+
+    @JsonCreator
+    public static Role getProperRole(String identifier) {
+        return Arrays.stream(Role.values())
+                .filter(role -> role.identifier.equals(identifier))
+                .findAny()
+                .orElse(null);
+    }
 }
