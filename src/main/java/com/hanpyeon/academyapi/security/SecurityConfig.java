1e24fbf2daadccd7a2bea149809069e6e4ee821e
huhdy32 <mong3268@gmail.com>
2024-02-02 16:45:45 +0900

    #18 refactor(SecurityConfig): deprecated 메소드 변경

--- src/main/java/com/hanpyeon/academyapi/security/SecurityConfig.java
+++ src/main/java/com/hanpyeon/academyapi/security/SecurityConfig.java
@@ -2,17 +2,11 @@ package com.hanpyeon.academyapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Random;
-import java.util.random.RandomGenerator;
 
 
 @EnableWebSecurity
@@ -21,11 +15,14 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()
+                .csrf(csrf ->
+                        csrf.disable()
+                )
                 .authorizeHttpRequests(http ->
                         http.anyRequest().permitAll()
                 ).build();
     }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2B);
