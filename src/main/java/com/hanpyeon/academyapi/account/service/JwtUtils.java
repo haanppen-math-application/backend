b40aceebb4f18f0b1eae2c7f40e0da422218e3b7
huhdy32 <mong3268@gmail.com>
2024-02-02 13:53:57 +0900

    #18 feat(JwtUtils): JWT 다루는 클래스 추가

---
+++ src/main/java/com/hanpyeon/academyapi/account/service/JwtUtils.java
@@ -0,0 +1,49 @@
+package com.hanpyeon.academyapi.account.service;
+
+import com.hanpyeon.academyapi.security.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+@Component
+public class JwtUtils {
+    private final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+    private final SecretKey SECRET_KEY = Keys.secretKeyFor(SIGNATURE_ALGORITHM);
+    private final String MEMBER_NAME = "memberName";
+    private final String MEMBER_ROLE = "memberRole";
+    private final long EXPIRATION_TIME = 1000 * 60 * 30; // 1초 -> 1분 -> 30분
+    private final JwtParser jwtParser = Jwts.parserBuilder()
+            .setSigningKey(SECRET_KEY)
+            .build();
+
+    public String generateToken(Long memberId, Role role, String name) {
+        Date time = new Date();
+        return Jwts.builder()
+                .setSubject(String.valueOf(memberId))
+                .claim(MEMBER_NAME, name)
+                .claim(MEMBER_ROLE, role)
+                .setIssuedAt(time)
+                .setExpiration(new Date(time.getTime() + EXPIRATION_TIME))
+                .signWith(SECRET_KEY, SIGNATURE_ALGORITHM)
+                .compact();
+    }
+
+    // 필터 에러 전역처리
+    public Claims parseToken(String token) {
+        return jwtParser.parseClaimsJws(token).getBody();
+    }
+    public Long getMemberId(Claims claims) {
+        return Long.parseLong(claims.getSubject());
+    }
+    public String getMemberName(Claims claims) {
+        return (String) claims.get(MEMBER_NAME);
+    }
+    public Role getMemberRole(Claims claims) {
+        return (Role) claims.get(MEMBER_ROLE);
+    }
+}
