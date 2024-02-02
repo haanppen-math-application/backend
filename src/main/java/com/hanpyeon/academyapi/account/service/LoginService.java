bd2fcab9a91aa75bebee63fc0f41b2103474626b
huhdy32 <mong3268@gmail.com>
2024-02-02 13:32:38 +0900

    #18 feat(LoginService): Jwt 제공 메소드 구현

---
+++ src/main/java/com/hanpyeon/academyapi/account/service/LoginService.java
@@ -0,0 +1,22 @@
+package com.hanpyeon.academyapi.account.service;
+
+import com.hanpyeon.academyapi.account.exceptions.NoSuchMemberException;
+import com.hanpyeon.academyapi.account.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+    private final JwtUtils jwtUtils;
+    @PostMapping
+    public String provideJwt(String phoneNumber) {
+        return memberRepository.findMemberByPhoneNumber(phoneNumber).stream()
+                .findAny()
+                .map(member ->
+                        jwtUtils.generateToken(member.getMemberId(), member.getUserRole(), member.getMemberName()))
+                .orElseThrow(NoSuchMemberException::new);
+    }
+}
