7cd9658ed83bd22c059367f3ff32c7cd9ac11eaa
huhdy32 <mong3268@gmail.com>
2024-02-02 12:44:20 +0900

    #18 fix(LoginController): 로그인 응답값 수정

--- src/main/java/com/hanpyeon/academyapi/account/controller/LoginController.java
+++ src/main/java/com/hanpyeon/academyapi/account/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.hanpyeon.academyapi.account.controller;
 
 import com.hanpyeon.academyapi.account.dto.LoginRequestDto;
 import com.hanpyeon.academyapi.account.service.LoginService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,14 +11,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/login")
+@RestController
+@RequestMapping("/api/login")
 @RequiredArgsConstructor
 public class LoginController {
     private final LoginService loginService;
     @PostMapping
-    public ResponseEntity<?> memberLogin(@RequestBody final LoginRequestDto loginRequestDto) {
+    public ResponseEntity<?> memberLogin(@Valid @RequestBody final LoginRequestDto loginRequestDto) {
         String token = loginService.provideJwt(loginRequestDto.userPhoneNumber());
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("Authentication", token);
