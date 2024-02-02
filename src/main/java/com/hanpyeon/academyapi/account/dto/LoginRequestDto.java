1180c42b6e0633454d163e14a8c4a323f310c1be
huhdy32 <mong3268@gmail.com>
2024-02-02 13:56:47 +0900

    #18 feat(LoginRequestDto): 전화번호 형식 검증 로직 추가

--- src/main/java/com/hanpyeon/academyapi/account/dto/LoginRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/account/dto/LoginRequestDto.java
@@ -2,9 +2,10 @@ package com.hanpyeon.academyapi.account.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record LoginRequestDto (
-        @NotBlank
+        @NotBlank @Pattern(regexp = "^[0-9]+$")
         String userPhoneNumber,
         @NotBlank
         String password
