2443035c18c72e8a7d23e936dee81dd045f8a8a3
huhdy32 <mong3268@gmail.com>
2024-02-02 16:35:06 +0900

    #18 test(JwtUtils): jwt 처리 유틸 클래스 테스트 케이스 추가

---
+++ src/test/java/com/hanpyeon/academyapi/account/service/JwtUtilsTest.java
@@ -0,0 +1,47 @@
+package com.hanpyeon.academyapi.account.service;
+
+import com.hanpyeon.academyapi.security.Role;
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtUtilsTest {
+    private JwtUtils jwtUtils;
+
+    @BeforeEach
+    void initJwtUtils() {
+        jwtUtils = new JwtUtils();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLegalArguments")
+    void generateToken(Long id, Role role, String name) {
+        String token = jwtUtils.generateToken(id, role, name);
+        assertThat(token).isNotBlank();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideLegalArguments")
+    void 토큰_추출_테스트(Long id, Role role, String name) {
+        String token = jwtUtils.generateToken(id, role, name);
+        Claims claims = jwtUtils.parseToken(token);
+        assertThat(jwtUtils.getMemberId(claims)).isEqualTo(Optional.ofNullable(id));
+        assertThat(jwtUtils.getMemberRole(claims)).isEqualTo(Optional.ofNullable(role));
+    }
+    static Stream<Arguments> provideLegalArguments() {
+        return Stream.of(
+                Arguments.of(null, null, null),
+                Arguments.of(null, Role.ROLE_MANAGER, "lqkwsmd"),
+                Arguments.of((long) 1, null, "lqkwsmd"),
+                Arguments.of((long) 1, Role.ROLE_MANAGER, null)
+                );
+    }
+}
