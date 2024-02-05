d99844d79a05b2b5e3daf0557996c533ad358ace
huhdy32 <mong3268@gmail.com>
2024-02-06 00:13:06 +0900

    #18 feat(SecurityConfig): JWT 필터 추가

--- src/main/java/com/hanpyeon/academyapi/security/SecurityConfig.java
+++ src/main/java/com/hanpyeon/academyapi/security/SecurityConfig.java
@@ -1,26 +1,39 @@
 package com.hanpyeon.academyapi.security;
 
+import com.hanpyeon.academyapi.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
+import java.util.List;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
         return httpSecurity
                 .csrf(csrf ->
                         csrf.disable()
                 )
+                .addFilterBefore(new JwtAuthenticationFilter(authenticationManager), AuthorizationFilter.class)
                 .authorizeHttpRequests(http ->
                         http.anyRequest().permitAll()
                 ).build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(List<AuthenticationProvider> providers) {
+        return new ProviderManager(providers);
     }
 
     @Bean
