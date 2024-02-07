6e918e2ac462226b1f99c330ace16a8df2c810e1
huhdy32 <101308287+huhdy32@users.noreply.github.com>
2024-02-07 15:28:02 +0900

    Revert "Develop"

--- src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
+++ src/main/java/com/hanpyeon/academyapi/AcademyApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcademyApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AcademyApplication.class, args);
     }
+
 }
--- src/main/java/com/hanpyeon/academyapi/security/Role.java
+++
@@ -1,7 +0,0 @@
-package com.hanpyeon.academyapi.security;
-
-public enum Role {
-    ROLE_STUDENT,
-    ROLE_TEACHER,
-    ROLE_MANAGER
-}
