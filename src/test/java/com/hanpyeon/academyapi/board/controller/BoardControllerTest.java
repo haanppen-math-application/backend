c187369a2cb00ea59ea44c22ca055f3b0ff09427
huhdy32 <mong3268@gmail.com>
2024-02-11 16:21:26 +0900

    #30 test(BoardControllerTest): 질문 게시판 컨트롤러 테스트 추가

---
+++ src/test/java/com/hanpyeon/academyapi/board/controller/BoardControllerTest.java
@@ -0,0 +1,109 @@
+package com.hanpyeon.academyapi.board.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanpyeon.academyapi.board.dto.QuestionRegisterRequestDto;
+import com.hanpyeon.academyapi.security.filter.JwtAuthenticationFilter;
+import org.apache.catalina.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+
+@WebMvcTest(controllers = BoardController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class, // 추가
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SecurityConfig.class, JwtAuthenticationFilter.class})
+        })
+class BoardControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void dto_없음_실패_테스트() throws Exception {
+        MockMultipartFile image = new MockMultipartFile(
+                "image",
+                "helwijadw".getBytes());
+        mockMvc.perform(multipart("/api/board")
+                .file(image)
+                .content(MediaType.MULTIPART_FORM_DATA_VALUE)
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+    @Test
+    void 이미지_없음_성공_테스트() throws Exception {
+        MockMultipartFile dto = new MockMultipartFile(
+                "questionRegisterRequestDto",
+                null,
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(new QuestionRegisterRequestDto(
+                        "제목",
+                        "내용",
+                        12L
+                )).getBytes()
+        );
+        mockMvc.perform(multipart("/api/board")
+                .file(dto)
+                .content(MediaType.MULTIPART_FORM_DATA_VALUE)
+        ).andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void 정상_형식_테스트() throws Exception {
+        MockMultipartFile image = new MockMultipartFile(
+                "image",
+                "helwijadw".getBytes());
+        MockMultipartFile dto = new MockMultipartFile(
+                "questionRegisterRequestDto",
+                null,
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(new QuestionRegisterRequestDto(
+                        "제목",
+                        "내용",
+                        12L
+                )).getBytes()
+        );
+
+        mockMvc.perform(multipart("/api/board")
+                .file(image)
+                .file(dto)
+                .content(MediaType.MULTIPART_FORM_DATA_VALUE)
+                ).andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void 여러_이미지_성공_테스트() throws Exception {
+        MockMultipartFile image1 = new MockMultipartFile(
+                "image",
+                "helwijadw".getBytes());
+        MockMultipartFile image2 = new MockMultipartFile(
+                "image",
+                "helwijadw".getBytes());
+
+        MockMultipartFile dto = new MockMultipartFile(
+                "questionRegisterRequestDto",
+                null,
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(new QuestionRegisterRequestDto(
+                        "제목",
+                        "내용",
+                        12L
+                )).getBytes()
+        );
+
+        mockMvc.perform(multipart("/api/board")
+                .file(image1)
+                .file(image2)
+                .file(dto)
+                .content(MediaType.MULTIPART_FORM_DATA_VALUE)
+        ).andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
