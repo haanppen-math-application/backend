30b890944a2d103fb785380f6ddde0396fe60627
huhdy32 <mong3268@gmail.com>
2024-02-13 18:21:23 +0900

    #30 feat: 질문 관련 멤버 조건 부 조회 기능 구현

---
+++ src/main/java/com/hanpyeon/academyapi/board/service/QuestionMemberProvider.java
@@ -0,0 +1,26 @@
+package com.hanpyeon.academyapi.board.service;
+
+import com.hanpyeon.academyapi.account.entity.Member;
+import com.hanpyeon.academyapi.board.dto.QuestionRegisterDto;
+import com.hanpyeon.academyapi.security.Role;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@AllArgsConstructor
+public class QuestionMemberProvider {
+    private final MemberManager memberManager;
+
+    public QuestionRelatedMember getQuestionRelatedMember(final QuestionRegisterDto questionDto) {
+        Member student = findMemberWithRoleValidation(questionDto.requestMemberId(), Role.STUDENT);
+        Member teacher = findMemberWithRoleValidation(questionDto.targetMemberId(), Role.MANAGER, Role.TEACHER);
+        return new QuestionRelatedMember(student, teacher);
+    }
+    private Member findMemberWithRoleValidation(final Long memberId, final Role... roles) {
+        return memberManager.getMemberWithValidated(
+                memberId, member -> Arrays.stream(roles).anyMatch(role -> role.equals(member.getRole()))
+        );
+    }
+}
