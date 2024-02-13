fd187e75436116083fbb07dfb3a15814e6cbf331
huhdy32 <mong3268@gmail.com>
2024-02-13 19:13:22 +0900

    #30 fix(Question): 단방향 관계로 새로 만들어지는 테이블 제거

--- src/main/java/com/hanpyeon/academyapi/board/entity/Question.java
+++ src/main/java/com/hanpyeon/academyapi/board/entity/Question.java
@@ -2,9 +2,12 @@ package com.hanpyeon.academyapi.board.entity;
 
 import com.hanpyeon.academyapi.account.entity.Member;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,14 +15,32 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CreationTimestamp
+    private LocalDateTime registeredDateTime;
     @Column(nullable = false)
-    private LocalDateTime registeredDate;
-    private String imagePath;
+    private String title;
+    @Column(nullable = false)
     private String content;
-    @Column(columnDefinition = "boolean default false")
-    private Boolean solved;
-    private Long viewCount;
+    @Column(nullable = false)
+    private Boolean solved = false;
+    @Column(nullable = false)
+    private Long viewCount = 0l;
     @ManyToOne(targetEntity = Member.class)
-    private Member member;
+    private Member ownerMember;
+    @ManyToOne(targetEntity = Member.class)
+    private Member targetMember;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "question_id")
+    private List<Image> images;
 
+
+    @Builder
+    private Question(LocalDateTime registeredDateTime, List<Image> images, String title, String content, Member ownerMember, Member targetMember) {
+        this.registeredDateTime = registeredDateTime;
+        this.images = images;
+        this.title = title;
+        this.content = content;
+        this.ownerMember = ownerMember;
+        this.targetMember = targetMember;
+    }
 }
