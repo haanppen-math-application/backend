24328916c18eff9231a19fb4c8ea07a7545f4b58
huhdy32 <mong3268@gmail.com>
2024-02-17 21:40:32 +0900

    #30 refactor: Image 엔티티 생성 책임 스토리지로부터 분리

--- src/main/java/com/hanpyeon/academyapi/board/service/ImageService.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/ImageService.java
@@ -1,39 +1,70 @@
 package com.hanpyeon.academyapi.board.service;
 
+import com.hanpyeon.academyapi.board.dto.ImageDto;
 import com.hanpyeon.academyapi.board.entity.Image;
 import com.hanpyeon.academyapi.board.repository.ImageRepository;
+import com.hanpyeon.academyapi.board.service.storage.MediaStorage;
+import com.hanpyeon.academyapi.board.service.storage.UploadFile;
+import com.hanpyeon.academyapi.board.service.validator.UploadImageValidator;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @Component
 @AllArgsConstructor
 public class ImageService {
+
     private final ImageRepository imageRepository;
-    private final StorageService storageService;
+    private final MediaStorage mediaStorage;
+    private final UploadImageValidator uploadImageValidator;
 
     /**
      * MultipartFile 을 처리하고, ImageRepository 엔티티 저장하는 역할을 수행합니다.
+     *
      * @param imageFiles empty일 경우, 빈 컬렉션을 리턴합니다.
      * @return images, never returns null
      */
     public List<Image> saveImage(final List<MultipartFile> imageFiles) {
-        if (imageFiles == null || imageFiles.isEmpty()) {
+        if (!checkImagesPresence(imageFiles)) {
             return Collections.emptyList();
         }
-        validate(imageFiles);
-        List<Image> images = storageService.storeMultiPartFiles(imageFiles);
-        return imageRepository.saveAll(images);
+        List<String> imageNames = new ArrayList<>();
+        for (MultipartFile multipartFile : imageFiles) {
+            UploadFile uploadFile = new UploadFile(multipartFile);
+            uploadFile.validateWith(uploadImageValidator);
+
+            imageNames.add(uploadFile.uploadTo(mediaStorage));
+        }
+        return saveImageNames(imageNames);
+    }
+    public ImageDto loadImage(final String imageName) {
+        Resource resource = mediaStorage.loadFile(imageName);
+        try {
+            return new ImageDto(resource, MediaType.parseMediaType(Files.probeContentType(resource.getFile().toPath())));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-
-    private void validate(List<MultipartFile> files) {
-        if (files.stream().anyMatch(Objects::isNull)) {
-            throw new IllegalArgumentException();
+    private boolean checkImagesPresence(final List<MultipartFile> imageFiles) {
+        if (imageFiles == null || imageFiles.isEmpty()) {
+            return false;
         }
+        return true;
+    }
+
+    private List<Image> saveImageNames(List<String> imageNames) {
+        List<Image> images = imageNames.stream()
+                .map(Image::new)
+                .toList();
+        return imageRepository.saveAll(images);
     }
 }
---
+++ src/main/java/com/hanpyeon/academyapi/board/service/storage/MediaStorage.java
@@ -0,0 +1,12 @@
+package com.hanpyeon.academyapi.board.service.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 외부 저장소에 이미지, 영상 데이터를 저장하고 가져오는 역할을 수행합니다.
+ */
+public interface MediaStorage {
+    String store(MultipartFile multipartFile, String newFileName);
+    Resource loadFile(String fileName);
+}
