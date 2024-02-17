03e2ba16c80270451da2e0be4bba33dbdd943a83
huhdy32 <mong3268@gmail.com>
2024-02-17 21:46:41 +0900

    #30 feat: 이미지 불러오기 API 구현

---
+++ src/main/java/com/hanpyeon/academyapi/board/controller/ImageController.java
@@ -0,0 +1,32 @@
+package com.hanpyeon.academyapi.board.controller;
+
+import com.hanpyeon.academyapi.board.dto.ImageDto;
+import com.hanpyeon.academyapi.board.service.ImageService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/media")
+public class ImageController {
+    private final ImageService imageService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/{imageSource}")
+    public ResponseEntity<?> getImage(@PathVariable("imageSource") String imageSource) throws IOException {
+        logger.info(imageSource + " ");
+        ImageDto media = imageService.loadImage(imageSource);
+
+        return ResponseEntity.ok()
+                .contentType(media.mediaType())
+                .body(media.data());
+    }
+}
