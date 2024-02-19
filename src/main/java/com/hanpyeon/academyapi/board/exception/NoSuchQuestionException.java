3988e3a99b751215198e3186a3fd243fd8430de0
huhdy32 <mong3268@gmail.com>
2024-02-19 14:10:17 +0900

    #32 feat(BoardService): 질문 상세조회 기능 구현

---
+++ src/main/java/com/hanpyeon/academyapi/board/exception/NoSuchQuestionException.java
@@ -0,0 +1,6 @@
+package com.hanpyeon.academyapi.board.exception;
+
+public class NoSuchQuestionException extends RuntimeException{
+    public NoSuchQuestionException() {
+    }
+}
--- src/main/java/com/hanpyeon/academyapi/board/service/BoardService.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/BoardService.java
@@ -1,8 +1,10 @@
 package com.hanpyeon.academyapi.board.service;
 
+import com.hanpyeon.academyapi.board.dto.QuestionDetails;
 import com.hanpyeon.academyapi.board.dto.QuestionRegisterDto;
 import com.hanpyeon.academyapi.board.entity.Image;
-import com.hanpyeon.academyapi.board.mapper.QuestionMapper;
+import com.hanpyeon.academyapi.board.exception.NoSuchQuestionException;
+import com.hanpyeon.academyapi.board.mapper.BoardMapper;
 import com.hanpyeon.academyapi.board.repository.QuestionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,16 +18,21 @@ public class BoardService {
     private final QuestionRepository questionRepository;
     private final QuestionRelatedMemberProvider questionRelatedMemberProvider;
     private final ImageService imageService;
-    private final QuestionMapper questionMapper;
+    private final BoardMapper boardMapper;
 
     public void addQuestion(@Validated final QuestionRegisterDto questionDto) {
         QuestionRelatedMember questionMember = questionRelatedMemberProvider.getQuestionRelatedMember(questionDto);
         List<Image> imageSources = imageService.saveImage(questionDto.images());
-        questionRepository.save(questionMapper.createEntity(
+        questionRepository.save(boardMapper.createEntity(
                 questionDto,
                 questionMember.requestMember(),
                 questionMember.targetMember(),
                 imageSources)
         );
+    }
+    public QuestionDetails getSingleQuestionDetails(final Long questionId) {
+        return questionRepository.findById(questionId)
+                .map(boardMapper::createQuestionDetails)
+                .orElseThrow(NoSuchQuestionException::new);
     }
 }
