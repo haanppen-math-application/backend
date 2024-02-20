e48ae6ce3a493702076c1843904501b0f74f2663
huhdy32 <mong3268@gmail.com>
2024-02-20 12:11:39 +0900

    #32 feat(QuestionRepository): Slice 반환 메소드 추가

--- src/main/java/com/hanpyeon/academyapi/board/repository/QuestionRepository.java
+++ src/main/java/com/hanpyeon/academyapi/board/repository/QuestionRepository.java
@@ -1,9 +1,12 @@
 package com.hanpyeon.academyapi.board.repository;
 
 import com.hanpyeon.academyapi.board.entity.Question;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Slice<Question> findBy(Pageable pageable);
 }
