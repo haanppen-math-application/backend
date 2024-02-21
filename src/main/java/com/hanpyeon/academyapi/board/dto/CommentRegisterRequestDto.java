001000ecc62d448a8dd83eb03478d832ffe50bc0
huhdy32 <mong3268@gmail.com>
2024-02-21 18:56:32 +0900

    #40 feat(CommentRegisterRequestDto): 대상 게시글 필드 추가

--- src/main/java/com/hanpyeon/academyapi/board/dto/CommentRegisterRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/board/dto/CommentRegisterRequestDto.java
@@ -3,6 +3,7 @@ package com.hanpyeon.academyapi.board.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record CommentRegisterRequestDto(
+        @NotNull Long questionId,
         String content
 ) {
 }
