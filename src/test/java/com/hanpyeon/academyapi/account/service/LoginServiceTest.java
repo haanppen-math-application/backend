eb6b1cd313a63db18bc99e5de645effeca9c5d0a
huhdy32 <mong3268@gmail.com>
2024-02-21 09:44:13 +0900

    #38 test(LoginServiceTest): 비밀번호 인증 성공 / 실패 테스트 케이스 추가

--- src/test/java/com/hanpyeon/academyapi/account/service/LoginServiceTest.java
+++ src/test/java/com/hanpyeon/academyapi/account/service/LoginServiceTest.java
@@ -1,9 +1,12 @@
 package com.hanpyeon.academyapi.account.service;
 
 import com.hanpyeon.academyapi.account.entity.Member;
+import com.hanpyeon.academyapi.account.exceptions.InvalidPasswordException;
 import com.hanpyeon.academyapi.account.exceptions.NoSuchMemberException;
 import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import com.hanpyeon.academyapi.security.JwtUtils;
+import com.hanpyeon.academyapi.security.PasswordHandler;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,11 +27,13 @@ class LoginServiceTest {
     Member member;
     @Mock
     JwtUtils jwtUtils;
+    @Mock
+    PasswordHandler passwordHandler;
     LoginService loginService;
 
     @BeforeEach
     void initLoginService() {
-        this.loginService = new LoginService(memberRepository, jwtUtils);
+        this.loginService = new LoginService(memberRepository, jwtUtils, passwordHandler);
     }
 
     @Test
@@ -36,15 +41,41 @@ class LoginServiceTest {
         Mockito.when(memberRepository.findMemberByPhoneNumber(Mockito.any()))
                 .thenReturn(Optional.empty());
         assertThatThrownBy(() -> {
-            loginService.provideJwt(Mockito.anyString());
+            loginService.provideJwt(Mockito.anyString(), Mockito.anyString());
         }).isInstanceOf(NoSuchMemberException.class);
     }
 
     @Test
-    void 사용자_찾기_성공_테스트() {
+    void 비밀번호_에러처리() {
+        member = Member.builder()
+                .password("010001010")
+                .password("12345")
+                .build();
+
         Mockito.when(memberRepository.findMemberByPhoneNumber(Mockito.anyString()))
                 .thenReturn(Optional.of(member));
-        Mockito.when(jwtUtils.generateToken(member.getId(), member.getRole(), member.getName())).thenReturn("12");
-        assertThat(loginService.provideJwt(Mockito.anyString())).isNotNull();
+        Mockito.when(passwordHandler.matches(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(false);
+
+        assertThatThrownBy(() -> loginService.provideJwt("1212", "12"))
+                .isInstanceOf(InvalidPasswordException.class);
+
+    }
+    @Test
+    void 비밀번호_성공처리() {
+        member = Member.builder()
+                .password("010001010")
+                .password("12345")
+                .build();
+
+        Mockito.when(memberRepository.findMemberByPhoneNumber(Mockito.anyString()))
+                .thenReturn(Optional.of(member));
+        Mockito.when(passwordHandler.matches(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(true);
+        Mockito.when(jwtUtils.generateToken(member.getId(), member.getRole(), member.getName()))
+                        .thenReturn("result");
+
+        assertThat(loginService.provideJwt("1212", "12"))
+                .isEqualTo("result");
     }
 }
