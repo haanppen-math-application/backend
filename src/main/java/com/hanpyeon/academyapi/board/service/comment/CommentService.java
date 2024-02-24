f034b9231fd17f09503951e4ac5fae76ed52e984
huhdy32 <mong3268@gmail.com>
2024-02-24 22:28:24 +0900

    #40 feat(CommentService): 댓글 수정 기능 구현

--- src/main/java/com/hanpyeon/academyapi/board/service/comment/CommentService.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/comment/CommentService.java
@@ -1,13 +1,15 @@
 package com.hanpyeon.academyapi.board.service.comment;
 
-import com.hanpyeon.academyapi.board.dto.CommentAdoptDto;
 import com.hanpyeon.academyapi.board.dto.CommentRegisterDto;
+import com.hanpyeon.academyapi.board.dto.CommentUpdateDto;
 import com.hanpyeon.academyapi.board.entity.Comment;
 import com.hanpyeon.academyapi.board.exception.NoSuchCommentException;
+import com.hanpyeon.academyapi.board.exception.RequestDeniedException;
 import com.hanpyeon.academyapi.board.repository.CommentRepository;
-import com.hanpyeon.academyapi.board.service.comment.adopt.CommentAdoptManager;
+import com.hanpyeon.academyapi.board.service.comment.content.CommentContentManager;
 import com.hanpyeon.academyapi.board.service.comment.register.CommentRegisterManager;
 import com.hanpyeon.academyapi.exception.ErrorCode;
+import com.hanpyeon.academyapi.media.service.ImageService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,8 @@ import org.springframework.validation.annotation.Validated;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentRegisterManager commentRegisterManager;
-    private final CommentAdoptManager commentAdoptManager;
+    private final CommentContentManager commentContentManager;
+    private final ImageService imageService;
 
     @Transactional
     public Long addComment(@Validated final CommentRegisterDto commentRegisterDto) {
@@ -28,13 +31,27 @@ public class CommentService {
     }
 
     @Transactional
-    public void adoptComment(@Validated final CommentAdoptDto commentAdoptDto) {
-        final Comment comment = findComment(commentAdoptDto.commentId());
-        commentAdoptManager.adopt(comment, commentAdoptDto.questionOwnerId());
+    public void updateComment(@Validated final CommentUpdateDto commentUpdateDto) {
+        final Comment comment = findComment(commentUpdateDto.commentId());
+        validateOwnedMember(comment, commentUpdateDto.requestMemberId());
+
+        if (commentUpdateDto.images() != null) {
+            imageService.updateImage(comment, commentUpdateDto.images());
+        }
+        if (commentUpdateDto.content() != null) {
+            commentContentManager.changeContentTo(comment, commentUpdateDto.content());
+        }
     }
 
     private Comment findComment(final Long commentId) {
         return commentRepository.findById(commentId)
                 .orElseThrow(() -> new NoSuchCommentException(ErrorCode.NO_SUCH_COMMENT));
+    }
+
+    private void validateOwnedMember(final Comment targetComment, final Long requestMemberId) {
+        final Long questionRegisteredMemberId = targetComment.getRegisteredMember().getId();
+        if (!questionRegisteredMemberId.equals(requestMemberId)) {
+            throw new RequestDeniedException("본인이 작성한 댓글이 아닙니다", ErrorCode.DENIED_EXCEPTION);
+        }
     }
 }
