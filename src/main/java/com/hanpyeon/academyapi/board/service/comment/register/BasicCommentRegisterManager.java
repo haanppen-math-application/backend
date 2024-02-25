032ef8602083e54d0d868914e4013f6e621c2116
huhdy32 <mong3268@gmail.com>
2024-02-26 08:59:40 +0900

    #40 feat(BasiCommentRegisterManager): 질문 게시판에 댓글은 학생만 달 수 있도록 구현

--- src/main/java/com/hanpyeon/academyapi/board/service/comment/register/BasicCommentRegisterManager.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/comment/register/BasicCommentRegisterManager.java
@@ -3,9 +3,12 @@ package com.hanpyeon.academyapi.board.service.comment.register;
 import com.hanpyeon.academyapi.account.entity.Member;
 import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import com.hanpyeon.academyapi.board.entity.Question;
+import com.hanpyeon.academyapi.board.exception.RequestDeniedException;
 import com.hanpyeon.academyapi.board.mapper.BoardMapper;
 import com.hanpyeon.academyapi.board.repository.QuestionRepository;
+import com.hanpyeon.academyapi.exception.ErrorCode;
 import com.hanpyeon.academyapi.media.service.ImageService;
+import com.hanpyeon.academyapi.security.Role;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,10 +18,14 @@ class BasicCommentRegisterManager extends AbstractCommentRegisterManager {
     }
 
     @Override
-    protected void verifyQuestion(Question question) {
+    protected void verifyQuestion(Question quesㅎtion) {
+
     }
 
     @Override
     protected void verifyMember(Member member) {
+        if (member.getRole().equals(Role.STUDENT)) {
+            throw new RequestDeniedException("학생은 질문에 대한 댓글을 달 수 없습니다", ErrorCode.DENIED_EXCEPTION);
+        }
     }
 }
