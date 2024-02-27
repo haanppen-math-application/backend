e82ac5c1a95f80f25099df618e6ca85920871908
huhdy32 <mong3268@gmail.com>
2024-02-27 14:16:11 +0900

    #40 feat(PasswordHandler): 비밀번호 외부 설정 파일 주입 설정

--- src/main/java/com/hanpyeon/academyapi/security/PasswordHandler.java
+++ src/main/java/com/hanpyeon/academyapi/security/PasswordHandler.java
@@ -1,12 +1,14 @@
 package com.hanpyeon.academyapi.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class PasswordHandler {
-    private static final String DEFAULT_PASSWORD = "0000";
+    @Value("service.password.default")
+    private String DEFAULT_PASSWORD;
     private final PasswordEncoder passwordEncoder;
 
     public PasswordHandler(final PasswordEncoder passwordEncoder) {
