71e8ccd6f30709ed13dfe1f4edf34e1d90ba78a5
huhdy32 <mong3268@gmail.com>
2024-02-28 09:46:11 +0900

    #48 feat(LogginAspect): warn, error 로깅을 위한 로직 구현

--- src/main/java/com/hanpyeon/academyapi/aspect/log/LoggingAspect.java
+++ src/main/java/com/hanpyeon/academyapi/aspect/log/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.hanpyeon.academyapi.aspect.log;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -22,5 +23,17 @@ public class LoggingAspect {
                 .map(Objects::toString)
                 .toList();
         log.info(" [ API REQUEST ] -> " + joinPoint.getSignature().getName() + ", [ WITH ARGUMENT ] -> " + arguments);
+    }
+
+    @AfterThrowing(value = "within(@WarnLoggable *)", throwing = "exception")
+    public void warningLog(JoinPoint joinPoint, Exception exception) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.error("[ " + exception.toString() + " ] -> " + exception.getMessage());
+    }
+
+    @AfterThrowing(value = "within(@ErrorLoggable *)", throwing = "exception")
+    public void errorLog(JoinPoint joinPoint, Exception exception) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.error("[ " + exception.toString() + " ] -> " + exception.getMessage());
     }
 }
