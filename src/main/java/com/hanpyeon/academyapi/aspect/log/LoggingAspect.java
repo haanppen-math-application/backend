2791175f1999df52322263524efa3b61f1695eb5
huhdy32 <mong3268@gmail.com>
2024-02-28 11:49:37 +0900

    #48 feat: 레포지토리 접근, 레포지토리 에러 로그 구현

--- src/main/java/com/hanpyeon/academyapi/aspect/log/LoggingAspect.java
+++ src/main/java/com/hanpyeon/academyapi/aspect/log/LoggingAspect.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -34,6 +35,25 @@ public class LoggingAspect {
     @AfterThrowing(value = "within(@ErrorLoggable *)", throwing = "exception")
     public void errorLog(JoinPoint joinPoint, Exception exception) {
         Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.error("[ " + exception.toString() + " ] -> " + exception.getMessage());
+    }
+
+    @Pointcut("execution(* com.hanpyeon.academyapi..*Repository.*(..))")
+    public void cutRepositoryAccess() {
+    }
+
+    @Before("cutRepositoryAccess()")
+    public void beforeAccess(JoinPoint joinPoint) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType());
+        List<String> arguments = Arrays.stream(joinPoint.getArgs())
+                .map(Objects::toString)
+                .toList();
+        logger.info(" [ REPOSITORY ACCESS ] -> " + joinPoint.getSignature().getName() + ", [ WITH ARGUMENT ] -> " + arguments);
+    }
+
+    @AfterThrowing(value = "cutRepositoryAccess()", throwing = "exception")
+    public void afterRepositoryThrows(JoinPoint joinPoint, Exception exception) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType());
         logger.error("[ " + exception.toString() + " ] -> " + exception.getMessage());
     }
 }
