f06f3f983c0d57ea6322278ea5a8aeb47fa9c148
huhdy32 <mong3268@gmail.com>
2024-03-01 08:50:09 +0900

    #50 feat(LoginRequestDto): Swagger API 문서 작업

--- src/main/java/com/hanpyeon/academyapi/account/dto/LoginRequestDto.java
+++ src/main/java/com/hanpyeon/academyapi/account/dto/LoginRequestDto.java
@@ -1,13 +1,15 @@
 package com.hanpyeon.academyapi.account.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
+@Schema(description = "로그인 정보")
 public record LoginRequestDto(
-        @NotBlank @Pattern(regexp = "^[0-9]+$")
+        @NotBlank @Pattern(regexp = "^[0-9]+$") @Schema(description = "전화번호",example = "01000000000")
         String userPhoneNumber,
-        @NotBlank
+        @NotBlank @Schema(description = "비밀번호", example = "0000")
         String password
 ) {
         @Override
