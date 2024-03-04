4ed0af8468567d9c5ba8aaeee98ca79752df4bfe
huhdy32 <mong3268@gmail.com>
2024-03-04 10:57:01 +0900

    #56 feat: 에러 시 WarnLog 발생시키도록 구현

--- src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionUpdateHandler.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionUpdateHandler.java
@@ -1,8 +1,10 @@
 package com.hanpyeon.academyapi.board.service.question.update;
 
+import com.hanpyeon.academyapi.aspect.log.WarnLoggable;
 import com.hanpyeon.academyapi.board.dto.QuestionUpdateDto;
 import com.hanpyeon.academyapi.board.entity.Question;
 
+@WarnLoggable
 public interface QuestionUpdateHandler {
     boolean applicable(QuestionUpdateDto questionUpdateDto);
     void update(Question question, QuestionUpdateDto questionUpdateDto);
--- src/main/java/com/hanpyeon/academyapi/board/service/question/validate/QuestionValidator.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/validate/QuestionValidator.java
@@ -1,7 +1,9 @@
 package com.hanpyeon.academyapi.board.service.question.validate;
 
+import com.hanpyeon.academyapi.aspect.log.WarnLoggable;
 import com.hanpyeon.academyapi.board.entity.Question;
 
+@WarnLoggable
 public interface QuestionValidator {
     void validate(Question question);
 }
