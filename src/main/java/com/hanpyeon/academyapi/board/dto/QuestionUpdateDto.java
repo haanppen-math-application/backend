e8f806fe4ae07ce49d58900b99a4462d9806b32c
huhdy32 <mong3268@gmail.com>
2024-03-04 10:00:37 +0900

    #56 fix(QuestionUpdateDto): dto 필수 값 변경

--- src/main/java/com/hanpyeon/academyapi/board/dto/QuestionUpdateDto.java
+++ src/main/java/com/hanpyeon/academyapi/board/dto/QuestionUpdateDto.java
@@ -1,6 +1,5 @@
 package com.hanpyeon.academyapi.board.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,17 +7,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Builder
-public record QuestionUpdateDto (
+public record QuestionUpdateDto(
         @NotNull
         Long questionId,
-        @NotBlank
         String content,
-        @NotNull
         Long targetMemberId,
         @NotNull
         Long requestMemberId,
         List<MultipartFile> images
-){
+) {
     @Override
     public String toString() {
         return "QuestionUpdateDto{" +
