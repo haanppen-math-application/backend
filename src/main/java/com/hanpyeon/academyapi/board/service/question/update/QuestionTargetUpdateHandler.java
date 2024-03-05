23354378dad6a137eea5f6b4185e6a679fadf6fe
huhdy32 <mong3268@gmail.com>
2024-03-05 21:48:09 +0900

    #56 refactor(QuestionTargetUpdateHandler): QuestionRelatedMemberProvider 클래스 삭제

--- src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionTargetUpdateHandler.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionTargetUpdateHandler.java
@@ -3,23 +3,27 @@ package com.hanpyeon.academyapi.board.service.question.update;
 import com.hanpyeon.academyapi.account.entity.Member;
 import com.hanpyeon.academyapi.board.dto.QuestionUpdateDto;
 import com.hanpyeon.academyapi.board.entity.Question;
-import com.hanpyeon.academyapi.board.service.question.register.QuestionRelatedMemberProvider;
+import com.hanpyeon.academyapi.board.service.question.register.MemberManager;
+import com.hanpyeon.academyapi.security.Role;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 @AllArgsConstructor
 class QuestionTargetUpdateHandler extends QuestionUpdateHandler {
-    private final QuestionRelatedMemberProvider questionRelatedMemberProvider;
+
+    private final MemberManager memberManager;
 
     @Override
-    boolean applicable(QuestionUpdateDto questionUpdateDto) {
-        return questionUpdateDto.targetMemberId() != null;
+    boolean applicable(final QuestionUpdateDto questionUpdateDto) {
+        return Objects.nonNull(questionUpdateDto.targetMemberId());
     }
 
     @Override
-    void process(Question question, QuestionUpdateDto questionUpdateDto) {
-        Member member = questionRelatedMemberProvider.getUpperTeacherTargetMember(questionUpdateDto.targetMemberId());
+    void process(final Question question, final QuestionUpdateDto questionUpdateDto) {
+        Member member = memberManager.getMemberWithRoleValidated(questionUpdateDto.targetMemberId(), Role.TEACHER, Role.MANAGER);
         question.changeTargetMember(member);
     }
 }
