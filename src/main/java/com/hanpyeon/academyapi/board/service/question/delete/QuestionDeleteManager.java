864ab481f4b5c7119386fa3a64bbfbf4182560c9
huhdy32 <mong3268@gmail.com>
2024-03-05 21:50:03 +0900

    #56 refactor(QuestionDeleteManager): 질문 삭제 로직 분리

---
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/delete/QuestionDeleteManager.java
@@ -0,0 +1,34 @@
+package com.hanpyeon.academyapi.board.service.question.delete;
+
+import com.hanpyeon.academyapi.board.entity.Question;
+import com.hanpyeon.academyapi.board.exception.RequestDeniedException;
+import com.hanpyeon.academyapi.board.service.question.register.MemberManager;
+import com.hanpyeon.academyapi.exception.ErrorCode;
+import com.hanpyeon.academyapi.media.service.ImageService;
+import com.hanpyeon.academyapi.security.Role;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Component
+@AllArgsConstructor
+public class QuestionDeleteManager {
+
+    private final MemberManager memberManager;
+    private final ImageService imageService;
+
+    @Transactional
+    public void delete(final Question question, final Long requestMemberId) {
+        validate(requestMemberId, question.getOwnerMember().getId());
+        imageService.removeImage(question.getImages());
+    }
+
+    private void validate(final Long requestMemberId, final Long questionOwnerId) {
+        if (requestMemberId.equals(questionOwnerId) || Objects.nonNull(memberManager.getMemberWithRoleValidated(requestMemberId, Role.MANAGER, Role.TEACHER))) {
+            return;
+        }
+        throw new RequestDeniedException(ErrorCode.DENIED_EXCEPTION);
+    }
+}
