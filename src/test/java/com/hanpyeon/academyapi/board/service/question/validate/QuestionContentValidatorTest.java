48d20006f3ff2ffc5884744d1e91a0ec5728cf21
huhdy32 <mong3268@gmail.com>
2024-03-05 11:16:03 +0900

    #56 test(QuestionValidatorTest): 질문 게시글 검증 로직 테스트

---
+++ src/test/java/com/hanpyeon/academyapi/board/service/question/validate/QuestionContentValidatorTest.java
@@ -0,0 +1,37 @@
+package com.hanpyeon.academyapi.board.service.question.validate;
+
+import com.hanpyeon.academyapi.board.entity.Question;
+import com.hanpyeon.academyapi.board.exception.QuestionContentOverSizeException;
+import com.hanpyeon.academyapi.board.exception.QuestionValidationException;
+import com.hanpyeon.academyapi.media.entity.Image;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class QuestionContentValidatorTest {
+
+    QuestionValidator questionValidator = new QuestionContentValidator();
+    @Test
+    void 글자수_초과_에러처리_테스트() {
+        String exceedSizeContent = "q".repeat(501);
+        final Question question = Question.builder().content(exceedSizeContent).build();
+
+        assertThatThrownBy(() -> questionValidator.validate(question))
+                .isInstanceOf(QuestionContentOverSizeException.class);
+    }
+    @Test
+    void 글자수_경계_테스트() {
+        String exceedSizeContent = "q".repeat(500);
+
+        final Question question = Question.builder().content(exceedSizeContent).build();
+
+        assertDoesNotThrow(() -> questionValidator.validate(question));
+    }
+}
