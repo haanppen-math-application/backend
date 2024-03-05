5d447b898fd0c11afdf92de91779a9765792f489
huhdy32 <mong3268@gmail.com>
2024-03-05 21:49:27 +0900

    #56 refactor(QuestionUpdateManager): 검증 로직 추가

--- src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionUpdateManager.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/update/QuestionUpdateManager.java
@@ -2,7 +2,9 @@ package com.hanpyeon.academyapi.board.service.question.update;
 
 import com.hanpyeon.academyapi.board.dto.QuestionUpdateDto;
 import com.hanpyeon.academyapi.board.entity.Question;
+import com.hanpyeon.academyapi.board.exception.RequestDeniedException;
 import com.hanpyeon.academyapi.board.service.question.validate.QuestionValidateManager;
+import com.hanpyeon.academyapi.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +13,19 @@ import java.util.List;
 @Component
 @AllArgsConstructor
 public class QuestionUpdateManager {
-    private final List<QuestionUpdateHandler> questionUpdateManagers;
+    private final List<QuestionUpdateHandler> questionUpdateHandlers;
     private final QuestionValidateManager questionValidateManager;
 
-    public void updateQuestion(final Question question, final QuestionUpdateDto questionUpdateDto) {
-        questionUpdateManagers.stream()
-                .forEach(questionUpdateHandler -> questionUpdateHandler.update(question, questionUpdateDto));
-        questionValidateManager.validate(question);
+    public void update(final Question targetQuestion, final QuestionUpdateDto questionUpdateDto) {
+        verifyAccess(targetQuestion.getOwnerMember().getId(), questionUpdateDto.requestMemberId());
+        questionUpdateHandlers.stream()
+                .forEach(questionUpdateHandler -> questionUpdateHandler.update(targetQuestion, questionUpdateDto));
+        questionValidateManager.validate(targetQuestion);
+    }
+
+    private void verifyAccess(final Long ownedMemberId, final Long requestMemberId) {
+        if (!requestMemberId.equals(ownedMemberId)) {
+            throw new RequestDeniedException("본인 질문이 아닙니다", ErrorCode.DENIED_EXCEPTION);
+        }
     }
 }
