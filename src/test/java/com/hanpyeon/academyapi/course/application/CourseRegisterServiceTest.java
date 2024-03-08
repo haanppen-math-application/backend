c9ead8df92cb95226b3bab88a19e7bc3a92e4cbe
huhdy32 <mong3268@gmail.com>
2024-03-08 19:46:15 +0900

    #59 test: 테스트 케이스 구성

---
+++ src/test/java/com/hanpyeon/academyapi/course/application/CourseRegisterServiceTest.java
@@ -0,0 +1,48 @@
+package com.hanpyeon.academyapi.course.application;
+
+import com.hanpyeon.academyapi.course.application.dto.CourseRegisterDto;
+import com.hanpyeon.academyapi.course.application.port.out.LoadStudentsPort;
+import com.hanpyeon.academyapi.course.application.port.out.LoadTeacherPort;
+import com.hanpyeon.academyapi.course.application.port.out.RegisterCoursePort;
+import com.hanpyeon.academyapi.course.domain.Student;
+import com.hanpyeon.academyapi.course.domain.Teacher;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CourseRegisterServiceTest {
+
+    @Mock
+    LoadStudentsPort loadStudentsPort;
+    @Mock
+    LoadTeacherPort loadTeacherPort;
+    @Mock
+    RegisterCoursePort registerCoursePort;
+
+    @InjectMocks
+    CourseRegisterService courseRegisterService;
+
+    @Test
+    void 등록_테스트() {
+        CourseRegisterDto courseRegisterDto = new CourseRegisterDto("1", null, null, null);
+
+
+        Mockito.when(registerCoursePort.register(Mockito.any()))
+                .thenReturn(1l);
+        Mockito.when(loadStudentsPort.loadStudent(Mockito.any()))
+                .thenReturn(List.of(new Student(1l)));
+        Mockito.when(loadTeacherPort.loadTeacher(Mockito.any()))
+                .thenReturn(new Teacher(1l));
+
+        assertThat(courseRegisterService.register(courseRegisterDto))
+                .isEqualTo(1l);
+    }
+}
