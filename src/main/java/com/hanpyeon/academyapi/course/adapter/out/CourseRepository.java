b5708ae962380d08c0a64793322bbbc3a4dcfae5
huhdy32 <mong3268@gmail.com>
2024-03-09 12:37:52 +0900

    #59 style(CourseRepository): CourseEntity 이름 변경

--- src/main/java/com/hanpyeon/academyapi/course/adapter/out/CourseRepository.java
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/CourseRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface CourseRepository extends JpaRepository<CourseEntity, Long> {
+interface CourseRepository extends JpaRepository<Course, Long> {
 }
