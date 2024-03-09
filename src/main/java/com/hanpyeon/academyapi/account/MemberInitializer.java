147795f2cf43d5447b0ec751aee17ff4ca15aaea
huhdy32 <mong3268@gmail.com>
2024-03-09 14:49:49 +0900

    #62 feat(MemberInitializer): 어드민 계정 추가 로직

---
+++ src/main/java/com/hanpyeon/academyapi/account/MemberInitializer.java
@@ -0,0 +1,35 @@
+package com.hanpyeon.academyapi.account;
+
+import com.hanpyeon.academyapi.account.entity.Member;
+import com.hanpyeon.academyapi.account.repository.MemberRepository;
+import com.hanpyeon.academyapi.security.PasswordHandler;
+import com.hanpyeon.academyapi.security.Role;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class MemberConfig {
+    private final MemberRepository memberRepository;
+    private final PasswordHandler passwordEncoder;
+
+    @Value("${application.admin.account.id}")
+    private String adminId;
+    @Value("${application.admin.account.password}")
+    private String adminPassword;
+
+    @PostConstruct
+    public void initAdminMember() {
+        memberRepository.save(Member.builder()
+                .phoneNumber(adminId)
+                .name("어드민계정")
+                .password(passwordEncoder.getEncodedPassword(adminPassword))
+                .role(Role.ADMIN)
+                .registeredDate(LocalDateTime.now())
+                .build());
+    }
+}
