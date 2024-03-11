d46047ba12949892636c3b63d3551298293f1690
huhdy32 <mong3268@gmail.com>
2024-03-11 14:45:25 +0900

    #hotfix(ErrorCode): 에러코드 식별값 중복 에러 수정

--- src/main/java/com/hanpyeon/academyapi/exception/ErrorCode.java
+++ src/main/java/com/hanpyeon/academyapi/exception/ErrorCode.java
@@ -19,17 +19,17 @@ public enum ErrorCode {
     NOT_SUPPORTED_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "-204", "등록 할 수 없는 사용자 유형"),
 
 
-    INVALID_MEMBER_TARGET(HttpStatus.BAD_REQUEST, "-101", "잘못된 대상입니다."),
-    NO_SUCH_MEMBER(HttpStatus.BAD_REQUEST, "-102", "존재하지 않는 사용자"),
-    NO_SUCH_QUESTION(HttpStatus.BAD_REQUEST, "-103", "존재하지 않는 질문 게시글"),
-    NO_SUCH_COMMENT(HttpStatus.BAD_REQUEST, "-104", "존재하지 않는 댓글"),
-    ALREADY_SOLVED_QUESTION_EXCEPTION(HttpStatus.BAD_REQUEST, "-105", "이미 해결된 질문"),
-    NOT_ADOPTED_COMMENT(HttpStatus.BAD_REQUEST, "-106", "채택되지 않은 댓글"),
-    QUESTION_CONTENT_OVERSIZE(HttpStatus.BAD_REQUEST, "-107", "질문 크기 초과"),
-    QUESTION_IMAGE_OVER_LENGTH(HttpStatus.BAD_REQUEST, "-108", "질문 게시글 이미지 갯수 초과"),
+    INVALID_MEMBER_TARGET(HttpStatus.BAD_REQUEST, "-301", "잘못된 대상입니다."),
+    NO_SUCH_MEMBER(HttpStatus.BAD_REQUEST, "-302", "존재하지 않는 사용자"),
+    NO_SUCH_QUESTION(HttpStatus.BAD_REQUEST, "-303", "존재하지 않는 질문 게시글"),
+    NO_SUCH_COMMENT(HttpStatus.BAD_REQUEST, "-304", "존재하지 않는 댓글"),
+    ALREADY_SOLVED_QUESTION_EXCEPTION(HttpStatus.BAD_REQUEST, "-305", "이미 해결된 질문"),
+    NOT_ADOPTED_COMMENT(HttpStatus.BAD_REQUEST, "-306", "채택되지 않은 댓글"),
+    QUESTION_CONTENT_OVERSIZE(HttpStatus.BAD_REQUEST, "-307", "질문 크기 초과"),
+    QUESTION_IMAGE_OVER_LENGTH(HttpStatus.BAD_REQUEST, "-308", "질문 게시글 이미지 갯수 초과"),
 
-    ILLEGAL_COURSE_NAME(HttpStatus.BAD_REQUEST, "-201", "잘못된 반 이름"),
-    ILLEGAL_COURSE_STUDENT_STATE(HttpStatus.BAD_REQUEST, "-202", "사용할 수 없는 학생"),
+    ILLEGAL_COURSE_NAME(HttpStatus.BAD_REQUEST, "-401", "잘못된 반 이름"),
+    ILLEGAL_COURSE_STUDENT_STATE(HttpStatus.BAD_REQUEST, "-402", "사용할 수 없는 학생"),
 
 
     INVALID_UPLOAD_FILE(HttpStatus.BAD_REQUEST, "-801", "파일 검증 실패"),
