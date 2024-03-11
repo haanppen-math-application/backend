194ea6f903385fc4f757a307293551b86485896e
huhdy32 <mong3268@gmail.com>
2024-03-11 12:21:57 +0900

    #feat(OpenApiConfig): 에러 코드 Swagger 문서화

---
+++ src/main/java/com/hanpyeon/academyapi/webconfig/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.hanpyeon.academyapi.webconfig;
+
+
+import com.hanpyeon.academyapi.exception.ErrorCode;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenApiCustomizer errorCodeSchemaCustomiser() {
+        return openApi -> {
+            Schema<ErrorCode[]> errorCodeSchema = new Schema<>();
+            Arrays.stream(ErrorCode.values()).forEach(errorCode -> {
+                Schema<?> errorSchema = new ObjectSchema()
+                        .addProperty("ERROR NAME", new StringSchema().example(errorCode))
+                        .addProperty("httpStatus", new StringSchema().example(String.valueOf(errorCode.getHttpStatus().value())))
+                        .addProperty("errorCode", new StringSchema().example(errorCode.getErrorCode()))
+                        .addProperty("description", new StringSchema().example(errorCode.getErrorDescription()));
+                errorCodeSchema.addProperty(errorCode.getErrorCode(), errorSchema);
+            });
+            openApi.getComponents().addSchemas("ErrorCode", errorCodeSchema);
+        };
+    }
+}
