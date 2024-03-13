f5818ef28c1a87acd9820b0639d594a3154556b7
huhdy32 <mong3268@gmail.com>
2024-03-13 13:49:48 +0900

    #hotfix(WebConfig): cors Credentional 허용

--- src/main/java/com/hanpyeon/academyapi/webconfig/WebConfig.java
+++ src/main/java/com/hanpyeon/academyapi/webconfig/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins("localhost")
+                .allowCredentials(true)
                 .allowedMethods("*");
     }
 }
