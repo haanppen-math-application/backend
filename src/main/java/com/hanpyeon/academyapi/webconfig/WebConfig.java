da12b169baa7184d903620f615b69286b9859ab1
huhdy32 <mong3268@gmail.com>
2024-03-13 23:38:13 +0900

    #feat(WebConfig): Authorization 헤더 허용

--- src/main/java/com/hanpyeon/academyapi/webconfig/WebConfig.java
+++ src/main/java/com/hanpyeon/academyapi/webconfig/WebConfig.java
@@ -3,6 +3,7 @@ package com.hanpyeon.academyapi.webconfig;
 import com.hanpyeon.academyapi.aspect.log.LogInterceptor;
 import jakarta.servlet.http.Cookie;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedHeaders("*")
+                .allowedHeaders(HttpHeaders.AUTHORIZATION)
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
