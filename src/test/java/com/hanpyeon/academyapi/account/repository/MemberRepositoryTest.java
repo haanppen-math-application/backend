8d86a6732dd690243bd8cdcc318517e2d65ae5f2
huhdy32 <mong3268@gmail.com>
2024-03-20 13:02:26 +0900

    #57 feat: 역할별 조회 API 구현

---
+++ src/test/java/com/hanpyeon/academyapi/account/repository/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.hanpyeon.academyapi.account.repository;
+
+import com.hanpyeon.academyapi.account.entity.Member;
+import com.hanpyeon.academyapi.security.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    void init() {
+        memberRepository.save(Member.builder()
+                .name("student")
+                .grade(11)
+                .phoneNumber("test")
+                .password("weqrew")
+                .registeredDate(LocalDateTime.now())
+                .role(Role.STUDENT)
+                .build());
+        memberRepository.save(Member.builder()
+                .name("student")
+                .grade(11)
+                .phoneNumber("test1")
+                .password("weqrew")
+                .registeredDate(LocalDateTime.now())
+                .role(Role.TEACHER)
+                .build());
+    }
+
+    @Test
+    void 역할_조회_테스트() {
+        assertThat(memberRepository.findMembersByRole(Role.STUDENT).size())
+                .isEqualTo(1);
+        assertThat(memberRepository.findMembersByRole(Role.TEACHER).size())
+                .isEqualTo(1);
+    }
+}
