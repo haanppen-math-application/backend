b20c83ccbc07588cf2ac3c31897a78ebe11043d9
huhdy32 <mong3268@gmail.com>
2024-03-21 17:42:29 +0900

    #53 feat(AccountPolicyManager): 계정 정책 적용 클래스 추가

--- src/main/java/com/hanpyeon/academyapi/account/service/verify/policy/AccountPolicy.java
+++ src/main/java/com/hanpyeon/academyapi/account/service/verify/policy/AccountPolicy.java
@@ -2,6 +2,6 @@ package com.hanpyeon.academyapi.account.service.verify.policy;
 
 import com.hanpyeon.academyapi.account.entity.Member;
 
-public interface AccountPolicy {
+interface AccountPolicy {
     void verify(final Member member);
 }
