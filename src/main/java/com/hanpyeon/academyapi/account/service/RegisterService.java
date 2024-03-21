b822af04f9819d5630d1cdecdc7cfbac8cc2a8ff
huhdy32 <mong3268@gmail.com>
2024-03-21 17:56:53 +0900

    #53 feat(RegisterService): 등록 요청 처리 로직 변경

--- src/main/java/com/hanpyeon/academyapi/account/service/RegisterService.java
+++ src/main/java/com/hanpyeon/academyapi/account/service/RegisterService.java
@@ -2,39 +2,24 @@ package com.hanpyeon.academyapi.account.service;
 
 import com.hanpyeon.academyapi.account.dto.RegisterMemberDto;
 import com.hanpyeon.academyapi.account.dto.RegisterMemberTotalDto;
-import com.hanpyeon.academyapi.account.exceptions.NotSupportedMemberTypeException;
 import com.hanpyeon.academyapi.account.mapper.RegisterMapper;
-import com.hanpyeon.academyapi.account.service.verify.MemberVerification;
+import com.hanpyeon.academyapi.account.service.verify.register.MemberRegisterRequestVerifyManager;
 import com.hanpyeon.academyapi.aspect.log.WarnLoggable;
-import com.hanpyeon.academyapi.exception.ErrorCode;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @WarnLoggable
 @AllArgsConstructor
 public class RegisterService {
-    private final static Logger LOGGER = LoggerFactory.getLogger("RegisterRoleChecker");
     private final RegisterServiceProvider registerServiceProvider;
-    private final List<MemberVerification> verifications;
+    private final MemberRegisterRequestVerifyManager registerRequestVerifyManager;
     private final RegisterMapper registerMapper;
     private final TimeProvider timeProvider;
 
     public void register(final RegisterMemberDto memberDto) {
-        verify(memberDto);
-        RegisterMemberTotalDto memberTotalDto = registerMapper.createMemberTotalDto(memberDto, timeProvider.getCurrTime());
+        registerRequestVerifyManager.verify(memberDto);
+        final RegisterMemberTotalDto memberTotalDto = registerMapper.createMemberTotalDto(memberDto, timeProvider.getCurrTime());
         registerServiceProvider.registerMember(memberTotalDto);
-    }
-
-    private void verify(final RegisterMemberDto memberDto) {
-        verifications.stream()
-                .filter(memberVerification -> memberVerification.supports(memberDto))
-                .findAny()
-                .orElseThrow(() -> new NotSupportedMemberTypeException(ErrorCode.NOT_SUPPORTED_MEMBER_TYPE))
-                .checkFields(memberDto);
     }
 }
