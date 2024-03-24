988498dc017a065539ed1464fc8ba942263d0948
huhdy32 <mong3268@gmail.com>
2024-03-24 11:49:45 +0900

    fix(RegisterServiceTest): 배포중 에러 수정

--- src/test/java/com/hanpyeon/academyapi/account/service/RegisterServiceTest.java
+++ src/test/java/com/hanpyeon/academyapi/account/service/RegisterServiceTest.java
@@ -2,21 +2,20 @@ package com.hanpyeon.academyapi.account.service;
 
 import com.hanpyeon.academyapi.account.dto.RegisterMemberDto;
 import com.hanpyeon.academyapi.security.Role;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@ExtendWith(MockitoExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class RegisterServiceTest {
     @Autowired
     RegisterService registerService;
