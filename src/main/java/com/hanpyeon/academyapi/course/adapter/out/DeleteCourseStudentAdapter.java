8fc14a0065024304bb6cb06467eb02aa00c2b685
huhdy32 <mong3268@gmail.com>
2024-04-01 10:26:52 +0900

    #78 feat(DeleteCOurseUseCase): 반 삭제 로직 초가

---
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/DeleteCourseStudentAdapter.java
@@ -0,0 +1,19 @@
+package com.hanpyeon.academyapi.course.adapter.out;
+
+import com.hanpyeon.academyapi.course.adapter.out.CourseStudentRepository;
+import com.hanpyeon.academyapi.course.application.port.out.DeleteCourseStudentPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+class DeleteCourseStudentAdapter implements DeleteCourseStudentPort {
+    private final CourseStudentRepository courseStudentRepository;
+    @Override
+    @Transactional
+    public void deleteCourseStudents(Long courseId) {
+        courseStudentRepository.deleteCourseStudentByCourseEntityId(courseId);
+    }
+}
