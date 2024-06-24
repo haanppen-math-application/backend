76a35ebb408343b00000031d7ed9d22af54c0d1d
huhdy32 <mong3268@gmail.com>
2024-06-24 12:28:15 +0900

    #88 feat(QueryMemberController): 이름을 이용한 조회 기능 추가

--- src/main/java/com/hanpyeon/academyapi/account/controller/QueryMemberController.java
+++ src/main/java/com/hanpyeon/academyapi/account/controller/QueryMemberController.java
@@ -7,9 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,19 +18,18 @@ public class QueryMemberController {
     private final QueryService queryService;
 
     @GetMapping("/teachers")
-    @Operation(summary = "전체 선생 조회 API", description = "인증된 사용자면 모두 가능")
+    @Operation(summary = "전체 선생 조회 API", description = "인증된 사용자면 모두 가능, ?name={찾고자 하는 이름} 시 검색, name이 없을시, 전체 조회")
     @SecurityRequirement(name = "jwtAuth")
-    public ResponseEntity<List<PreviewTeacher>> queryTeachers() {
-        List<PreviewTeacher> teachers = queryService.loadTeachers();
+    public ResponseEntity<List<PreviewTeacher>> queryTeachers(@RequestParam(defaultValue = "null") String name) {
+        List<PreviewTeacher> teachers = queryService.loadTeachers(name);
         return ResponseEntity.ok(teachers);
     }
 
     @GetMapping("/students")
     @Operation(summary = "전체 학생 조회 API", description = "인증된 사용자면 모두 가능")
     @SecurityRequirement(name = "jwtAuth")
-    public ResponseEntity<List<PreviewStudent>> queryStudents() {
-        List<PreviewStudent> students = queryService.loadStudents();
+    public ResponseEntity<List<PreviewStudent>> queryStudents(@RequestParam(defaultValue = "null") String name) {
+        List<PreviewStudent> students = queryService.loadStudents(name);
         return ResponseEntity.ok(students);
     }
-
 }
--- src/main/java/com/hanpyeon/academyapi/account/service/QueryService.java
+++ src/main/java/com/hanpyeon/academyapi/account/service/QueryService.java
@@ -7,27 +7,37 @@ import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import com.hanpyeon.academyapi.security.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QueryService {
-
     private final MemberRepository memberRepository;
 
-    public List<PreviewTeacher> loadTeachers() {
+    public List<PreviewTeacher> loadTeachers(final String name) {
         List<Member> teacherEntities = memberRepository.findMembersByRole(Role.TEACHER);
+        if (name.equals("null")) {
+            return teacherEntities.stream()
+                    .map(PreviewTeacher::of)
+                    .toList();
+        }
         return teacherEntities.stream()
+                .filter(teacher -> teacher.getName().contains(name))
                 .map(PreviewTeacher::of)
                 .toList();
     }
 
-    public List<PreviewStudent> loadStudents() {
+    public List<PreviewStudent> loadStudents(final String name) {
         List<Member> studentEntities = memberRepository.findMembersByRole(Role.STUDENT);
-        return studentEntities.stream()
+        if (name.equals("null")) {
+            return studentEntities.stream()
+                    .map(PreviewStudent::of)
+                    .toList();
+        }
+        return studentEntities.stream().filter(student -> student.getName().contains(name))
                 .map(PreviewStudent::of)
                 .toList();
     }
-
 }
