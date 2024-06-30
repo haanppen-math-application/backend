f8a0ce39284fa679a8d7ec3c44a2f1031e54a1d7
huhdy32 <mong3268@gmail.com>
2024-06-30 20:56:39 +0900

    #95 fix(MemberPhoneNumberPolicy): 계정 비밀번호 유효성 로직 추가

--- src/main/java/com/hanpyeon/academyapi/account/service/verify/policy/MemberPhoneNumberPolicy.java
+++ src/main/java/com/hanpyeon/academyapi/account/service/verify/policy/MemberPhoneNumberPolicy.java
@@ -3,6 +3,7 @@ package com.hanpyeon.academyapi.account.service.verify.policy;
 import com.hanpyeon.academyapi.account.entity.Member;
 import com.hanpyeon.academyapi.account.exceptions.InvalidAccountPolicy;
 import com.hanpyeon.academyapi.exception.ErrorCode;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +15,12 @@ class MemberPhoneNumberPolicy implements AccountPolicy {
         final String phoneNumber = member.getPhoneNumber();
         if (!phoneNumber.startsWith(PHONE_NUMBER_STARTS_POLICY)) {
             throw new InvalidAccountPolicy("전화번호는 " + PHONE_NUMBER_STARTS_POLICY + " 로 시작", ErrorCode.ACCOUNT_POLICY);
+        }
+        if (!phoneNumber.matches("^[0-9]+$")) {
+            throw new InvalidAccountPolicy("전화번호는 숫자여야 합니다.", ErrorCode.ACCOUNT_POLICY);
+        }
+        if (phoneNumber.length() != 11) {
+            throw new InvalidAccountPolicy("전화번호는 11자리 숫자여야 합니다", ErrorCode.ACCOUNT_POLICY);
         }
     }
 }
