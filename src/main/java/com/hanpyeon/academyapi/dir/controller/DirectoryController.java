b1e62e93d2c95c0b9fb6b353d488e99429bb7948
huhdy32 <mong3268@gmail.com>
2024-08-16 20:34:52 +0900

    #fix(DirectoryController): 디렉토리 생성시 공개 여부 추가

--- src/main/java/com/hanpyeon/academyapi/dir/controller/DirectoryController.java
+++ src/main/java/com/hanpyeon/academyapi/dir/controller/DirectoryController.java
@@ -5,8 +5,10 @@ import com.hanpyeon.academyapi.dir.service.DirectoryService;
 import com.hanpyeon.academyapi.security.authentication.MemberPrincipal;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,14 +28,15 @@ public class DirectoryController {
             @AuthenticationPrincipal final MemberPrincipal memberPrincipal
     ) {
 
-        final CreateDirectoryDto createDirectoryDto = new CreateDirectoryDto(createDirectoryRequest.directoryName(), createDirectoryRequest.directoryPath(), memberPrincipal.memberId());
-        directoryService.createNewDirectory(createDirectoryDto);
+        final CreateDirectoryDto createDirectoryDto = new CreateDirectoryDto(createDirectoryRequest.directoryName(), createDirectoryRequest.directoryPath(), memberPrincipal.memberId(), createDirectoryRequest.canViewByEveryone());
+        directoryService.addNewDirectory(createDirectoryDto);
         return ResponseEntity.ok(null);
     }
 
     record CreateDirectoryRequest(
             @NotBlank String directoryPath,
-            @NotBlank String directoryName
+            @NotBlank String directoryName,
+            @NotNull Boolean canViewByEveryone
     ) {
     }
 }
