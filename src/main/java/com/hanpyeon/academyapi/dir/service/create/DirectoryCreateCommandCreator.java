3055fb9b29cff70e8bb32dc8cdaf08bf7f1ef624
huhdy32 <mong3268@gmail.com>
2024-08-22 00:51:30 +0900

    #refactor(DirectoryCreateCommandCreator): 경로 생성 로직 분리

--- src/main/java/com/hanpyeon/academyapi/dir/service/create/DirectoryCreateCommandCreator.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/create/DirectoryCreateCommandCreator.java
@@ -5,6 +5,7 @@ import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import com.hanpyeon.academyapi.dir.dto.CreateDirectoryCommand;
 import com.hanpyeon.academyapi.dir.dto.CreateDirectoryDto;
 import com.hanpyeon.academyapi.dir.exception.DirectoryException;
+import com.hanpyeon.academyapi.dir.service.form.resolver.DirectoryPathFormResolver;
 import com.hanpyeon.academyapi.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,25 +15,13 @@ import org.springframework.validation.annotation.Validated;
 @RequiredArgsConstructor
 public class DirectoryCreateCommandCreator {
     private final MemberRepository memberRepository;
+    private final DirectoryPathFormResolver directoryPathFormResolver;
 
     public CreateDirectoryCommand create(@Validated final CreateDirectoryDto createDirectoryDto) {
-        final String dirPath = getDirPath(createDirectoryDto.directoryPath());
+        final String parentDirPath = directoryPathFormResolver.resolveToAbsolutePath(createDirectoryDto.parentDirPath());
         final Member member = memberRepository.findMemberByIdAndRemovedIsFalse(createDirectoryDto.ownerId())
                 .orElseThrow(() -> new DirectoryException(ErrorCode.CANNOT_FIND_USER));
-        return new CreateDirectoryCommand(dirPath, createDirectoryDto.directoryName(), getAbsolutePath(dirPath, createDirectoryDto.directoryName()), createDirectoryDto.canViewByEveryone(), member);
-    }
-
-    private String getDirPath(final String dirPath) {
-        if (dirPath.contains("//")) {
-            throw new DirectoryException(dirPath, ErrorCode.ILLEGAL_PATH);
-        }
-        if (dirPath.charAt(dirPath.length() - 1) != '/') {
-            return dirPath + "/";
-        }
-        return dirPath;
-    }
-
-    private String getAbsolutePath(final String dirPath, final String dirName) {
-        return dirPath + dirName + "/";
+        final String absoluteDirPath = directoryPathFormResolver.resolveToAbsolutePath(parentDirPath, createDirectoryDto.directoryName());
+        return new CreateDirectoryCommand(parentDirPath, createDirectoryDto.directoryName(), absoluteDirPath, createDirectoryDto.canViewByEveryone(), member);
     }
 }
---
+++ src/main/java/com/hanpyeon/academyapi/dir/service/form/validate/DirectoryPathFormValidator.java
@@ -0,0 +1,5 @@
+package com.hanpyeon.academyapi.dir.service.form.validate;
+
+public interface DirectoryPathFormValidator {
+    void validateDirPath(final String dirPath);
+}
