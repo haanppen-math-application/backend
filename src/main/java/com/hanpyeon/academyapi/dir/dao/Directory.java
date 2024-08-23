6058ece47aae35221991147e529943f2cd5ec945
huhdy32 <mong3268@gmail.com>
2024-08-23 10:24:37 +0900

    #feat(Directory): 디렉토리 엔티티 및 레포지토리 구성

--- src/main/java/com/hanpyeon/academyapi/dir/dao/Directory.java
+++ src/main/java/com/hanpyeon/academyapi/dir/dao/Directory.java
@@ -1,17 +1,21 @@
 package com.hanpyeon.academyapi.dir.dao;
 
 import com.hanpyeon.academyapi.account.entity.Member;
+import com.hanpyeon.academyapi.media.entity.Media;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(indexes = @Index(name = "idx_path", columnList = "directory_path"))
 @NoArgsConstructor
 @Getter
+@ToString
 public class Directory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,14 +25,19 @@ public class Directory {
     private Member owner;
     @Column(name = "directory_path", nullable = false, unique = true)
     private String path;
-    @Column(name = "directory_opened", nullable = false)
+    @Column(name = "directory_modifiable", nullable = false)
     private Boolean canViewByEveryone;
+    @Column(name = "directory_viewable", nullable = false)
+    private Boolean canModifyByEveryone;
     @CreationTimestamp
     private LocalDateTime createdTime;
+    @OneToMany
+    private List<Media> medias;
 
-    public Directory(Member owner, String path, Boolean canViewByEveryone) {
+    public Directory(Member owner, String path, Boolean canModifyByEveryone, Boolean canViewByEveryone) {
         this.owner = owner;
         this.path = path;
+        this.canModifyByEveryone = canModifyByEveryone;
         this.canViewByEveryone = canViewByEveryone;
     }
 }
--- src/main/java/com/hanpyeon/academyapi/dir/dao/DirectoryRepository.java
+++ src/main/java/com/hanpyeon/academyapi/dir/dao/DirectoryRepository.java
@@ -1,11 +1,16 @@
 package com.hanpyeon.academyapi.dir.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DirectoryRepository extends JpaRepository<Directory, Long> {
     Optional<Directory> findDirectoryByPath(final String path);
+    @Query("SELECT e FROM Directory e WHERE e.path LIKE :dirPath AND e.path NOT LIKE :excludePath")
+    List<Directory> queryDirectoriesByPath(@Param("dirPath") final String dirPath, @Param("excludePath") final String excludePath);
 }
