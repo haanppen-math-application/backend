b67bf034b9268c478b8bcc452b0e551c57da3dba
huhdy32 <mong3268@gmail.com>
2024-08-23 10:24:06 +0900

    #feat(DirectoryService): 디렉토리 조회 서비스 클래스 구성

--- src/main/java/com/hanpyeon/academyapi/dir/controller/DirectoryController.java
+++ src/main/java/com/hanpyeon/academyapi/dir/controller/DirectoryController.java
@@ -1,19 +1,21 @@
 package com.hanpyeon.academyapi.dir.controller;
 
 import com.hanpyeon.academyapi.dir.dto.CreateDirectoryDto;
+import com.hanpyeon.academyapi.dir.dto.FileView;
+import com.hanpyeon.academyapi.dir.dto.QueryDirectoryDto;
 import com.hanpyeon.academyapi.dir.service.DirectoryService;
 import com.hanpyeon.academyapi.security.authentication.MemberPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/directories")
@@ -23,20 +25,30 @@ public class DirectoryController {
     private final DirectoryService directoryService;
 
     @PostMapping
+    @Operation(summary = "디렉토리를 새로 생성하는 api 입니다", description = "기본 디렉토리로 /, /teachers 가 존재합니다")
     public ResponseEntity<?> createDirectory(
             @RequestBody @Valid final CreateDirectoryRequest createDirectoryRequest,
             @AuthenticationPrincipal final MemberPrincipal memberPrincipal
     ) {
-
-        final CreateDirectoryDto createDirectoryDto = new CreateDirectoryDto(createDirectoryRequest.directoryName(), createDirectoryRequest.directoryPath(), memberPrincipal.memberId(), createDirectoryRequest.canViewByEveryone());
+        final CreateDirectoryDto createDirectoryDto = new CreateDirectoryDto(createDirectoryRequest.directoryName(), createDirectoryRequest.directoryPath(), memberPrincipal.memberId(), createDirectoryRequest.canViewByEveryone(), createDirectoryRequest.canModifyByEveryone());
         directoryService.addNewDirectory(createDirectoryDto);
         return ResponseEntity.ok(null);
     }
 
+    @GetMapping
+    @Operation(summary = "디렉토리를 조회하는 api 입니다", description = "dirPath를 작성하지 않을 시, / 의 디렉토리를 조회합니다")
+    public ResponseEntity<List<FileView>> queryCurrDirPath(
+            @RequestParam(defaultValue = "/") final String dirPath,
+            @AuthenticationPrincipal final MemberPrincipal memberPrincipal
+    ) {
+        return ResponseEntity.ok(directoryService.loadCurrFiles(new QueryDirectoryDto(dirPath, memberPrincipal.memberId())));
+    }
+
     record CreateDirectoryRequest(
-            @NotBlank String directoryPath,
-            @NotBlank String directoryName,
-            @NotNull Boolean canViewByEveryone
+            @NotBlank @Pattern(regexp = "^/.*$") String directoryPath,
+            @NotBlank @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$") String directoryName,
+            @NotNull Boolean canViewByEveryone,
+            @NotNull Boolean canModifyByEveryone
     ) {
     }
 }
--- src/main/java/com/hanpyeon/academyapi/dir/service/DirectoryService.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/DirectoryService.java
@@ -4,12 +4,17 @@ import com.hanpyeon.academyapi.dir.dao.Directory;
 import com.hanpyeon.academyapi.dir.dao.DirectoryRepository;
 import com.hanpyeon.academyapi.dir.dto.CreateDirectoryCommand;
 import com.hanpyeon.academyapi.dir.dto.CreateDirectoryDto;
+import com.hanpyeon.academyapi.dir.dto.FileView;
+import com.hanpyeon.academyapi.dir.dto.QueryDirectoryDto;
 import com.hanpyeon.academyapi.dir.service.create.DirectoryCreateCommandCreator;
 import com.hanpyeon.academyapi.dir.service.create.DirectoryCreator;
-import jakarta.validation.Valid;
+import com.hanpyeon.academyapi.dir.service.query.DirectoryQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +22,17 @@ public class DirectoryService {
     private final DirectoryCreator directoryCreator;
     private final DirectoryCreateCommandCreator directoryCreateCommandCreator;
     private final DirectoryRepository directoryRepository;
+    private final DirectoryQueryService directoryQueryService;
 
     @Transactional
-    public void addNewDirectory(@Valid final CreateDirectoryDto createDirectoryDto) {
+    public void addNewDirectory(@Validated final CreateDirectoryDto createDirectoryDto) {
         final CreateDirectoryCommand createDirectoryCommand = directoryCreateCommandCreator.create(createDirectoryDto);
         final Directory directory = directoryCreator.createDirectory(createDirectoryCommand);
         directoryRepository.save(directory);
+    }
+
+    @Transactional(readOnly = true)
+    public List<FileView> loadCurrFiles(final QueryDirectoryDto queryDirectoryDto) {
+        return directoryQueryService.queryDirectory(queryDirectoryDto);
     }
 }
