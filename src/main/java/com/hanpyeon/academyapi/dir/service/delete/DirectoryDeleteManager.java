631367df9f2bba217eeabc24afbe9682b8e5c214
huhdy32 <mong3268@gmail.com>
2024-08-26 21:02:48 +0900

    #86 feat(DeleteDirectoryProcessor): DeleteDirectoryHandler들을 주입받아 사용자 권한 별로 처리로직 실행

--- src/main/java/com/hanpyeon/academyapi/dir/service/delete/DirectoryDeleteManager.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/delete/DirectoryDeleteManager.java
@@ -1,6 +1,6 @@
 package com.hanpyeon.academyapi.dir.service.delete;
 
-import com.hanpyeon.academyapi.dir.service.delete.executor.DeleteDirectoryExecutor;
+import com.hanpyeon.academyapi.dir.service.delete.executor.DeleteDirectoryProcessor;
 import com.hanpyeon.academyapi.dir.service.delete.validate.DirectoryDeleteValidateManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service;
 class DirectoryDeleteManager {
 
     private final DirectoryDeleteValidateManager directoryDeleteValidateManager;
-    private final DeleteDirectoryExecutor deleteDirectoryExecutor;
+    private final DeleteDirectoryProcessor deleteDirectoryProcessor;
 
     public void delete(final DirectoryDeleteCommand directoryDeleteCommand) {
         directoryDeleteValidateManager.validate(directoryDeleteCommand);
-        deleteDirectoryExecutor.delete(directoryDeleteCommand);
+        deleteDirectoryProcessor.delete(directoryDeleteCommand);
     }
 }
---
+++ src/main/java/com/hanpyeon/academyapi/dir/service/delete/executor/DeleteDirectoryProcessor.java
@@ -0,0 +1,23 @@
+package com.hanpyeon.academyapi.dir.service.delete.executor;
+
+import com.hanpyeon.academyapi.dir.exception.DirectoryException;
+import com.hanpyeon.academyapi.dir.service.delete.DirectoryDeleteCommand;
+import com.hanpyeon.academyapi.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteDirectoryProcessor {
+    private final List<DeleteDirectoryHandler> deleteDirectoryExecutors;
+
+    public Integer delete(DirectoryDeleteCommand directoryDeleteCommand) {
+        return deleteDirectoryExecutors.stream()
+                .filter(deleteDirectoryExecutor -> deleteDirectoryExecutor.applicable(directoryDeleteCommand.getRequestMember().getRole()))
+                .findFirst()
+                .orElseThrow(() -> new DirectoryException(directoryDeleteCommand.getRequestMember().getRole() + "사용자는 디렉토리를 삭제할 수 없습니다.", ErrorCode.DIRECTORY_CANNOT_DELETE))
+                .process(directoryDeleteCommand);
+    }
+}
