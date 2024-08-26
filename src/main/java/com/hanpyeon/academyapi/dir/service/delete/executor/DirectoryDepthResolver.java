6cc7c1a4bb27e49cad07095a97d23efe593ffffe
huhdy32 <mong3268@gmail.com>
2024-08-26 21:07:27 +0900

    #86 feat(DirectoryDepthResolver): 디렉토리 depth를 계산해주는 클래스 구성

---
+++ src/main/java/com/hanpyeon/academyapi/dir/service/delete/executor/DirectoryDepthResolver.java
@@ -0,0 +1,33 @@
+package com.hanpyeon.academyapi.dir.service.delete.executor;
+
+import com.hanpyeon.academyapi.dir.dao.Directory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+class DirectoryDepthResolver {
+    public List<Directory> getDecsendingList(final List<Directory> directories) {
+        return directories.stream()
+                .map(directory -> new DirectoryDepth(directory, this.getDepth(directory.getPath())))
+                .sorted((directory1, directory2) -> directory2.depth() - directory1.depth())
+                .map(directoryDepth -> directoryDepth.directory)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Integer getDepth(final String path) {
+        int depthCount = 0;
+        for (int i = 0; i < path.length() - 1; i++) {
+            if (path.charAt(i) == '/') {
+                depthCount++;
+            }
+        }
+        return depthCount;
+    }
+
+    private record DirectoryDepth(
+            Directory directory,
+            Integer depth) {
+    }
+}
