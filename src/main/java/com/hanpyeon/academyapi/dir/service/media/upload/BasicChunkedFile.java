be783f3f2047ac51684dcc71be11799f667bf71f
huhdy32 <mong3268@gmail.com>
2024-08-31 00:10:27 +0900

    #86 feat(BasicChunkedFile): 청크 파일 하나당 chunkId가 하나만 증가하도록 하기 위해 싱글톤 패턴 응용

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/BasicChunkedFile.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/BasicChunkedFile.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 class BasicChunkedFile implements ChunkedFile {
@@ -14,6 +15,7 @@ class BasicChunkedFile implements ChunkedFile {
     private final MultipartFile multipartFile;
     private final ChunkGroupInfo chunkGroupInfo;
     private final Boolean isLast;
+    private String uniqueChunkName = null;
 
     @Override
     public ChunkGroupInfo getChunkGroupInfo() {
@@ -27,7 +29,10 @@ class BasicChunkedFile implements ChunkedFile {
 
     @Override
     public String getUniqueFileName() {
-        return chunkGroupInfo.getChunkUniqueId();
+        if (Objects.isNull(this.uniqueChunkName)) {
+            this.uniqueChunkName = this.chunkGroupInfo.getChunkUniqueId();
+        }
+        return uniqueChunkName;
     }
 
     @Override
