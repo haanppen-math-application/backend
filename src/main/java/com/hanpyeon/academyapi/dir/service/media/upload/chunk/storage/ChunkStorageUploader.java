2c3c04a71b01094e0ac1a925a567404977aef84f
huhdy32 <mong3268@gmail.com>
2024-09-02 23:14:03 +0900

    #86 refactor(ChunkStorageUploader): 청크 파일 업로드 로직 ChunkedFile 을 사용하도록 수정

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/storage/ChunkStorageUploader.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/storage/ChunkStorageUploader.java
@@ -1,6 +1,5 @@
 package com.hanpyeon.academyapi.dir.service.media.upload.chunk.storage;
 
-import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkGroupInfo;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkedFile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,6 @@ public class ChunkStorageUploader {
 
     public void upload(final ChunkedFile chunkedFile, final ChunkStorage chunkStorage) {
         chunkStorage.save(chunkedFile);
-        final ChunkGroupInfo info = chunkedFile.getChunkGroupInfo();
-        info.updateGroupIndex(chunkedFile.getChunkSize());
+        chunkedFile.updateCurrentInfoChunkIndex();
     }
 }
