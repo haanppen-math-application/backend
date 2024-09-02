a84ee3bd70a3fef6afc1f76ee292a7352d4e2372
huhdy32 <mong3268@gmail.com>
2024-09-02 23:36:39 +0900

    #86 feat(RequireNextChunk): 아직 도착하지 않은 청크 크기 표현할수 있도록 구현

--- src/main/java/com/hanpyeon/academyapi/dir/dto/RequireNextChunk.java
+++ src/main/java/com/hanpyeon/academyapi/dir/dto/RequireNextChunk.java
@@ -4,19 +4,21 @@ import lombok.Getter;
 
 @Getter
 public class RequireNextChunk {
-    private final Long requiringNextChunkIndex;
+    private final Long nextChunkIndex;
+    private final Long remainSize;
     private final Boolean needMore;
 
-    private RequireNextChunk(final Long requiringNextChunkIndex, final Boolean needMore) {
-        this.requiringNextChunkIndex = requiringNextChunkIndex;
+    private RequireNextChunk(final Long nextChunkIndex, final Long remainSize, final Boolean needMore) {
+        this.nextChunkIndex = nextChunkIndex;
+        this.remainSize = remainSize;
         this.needMore = needMore;
     }
 
-    public static RequireNextChunk needMore(final Long requiringChunkIndex) {
-        return new RequireNextChunk(requiringChunkIndex, true);
+    public static RequireNextChunk needMore(final Long requiringChunkIndex, final Long remainSize) {
+        return new RequireNextChunk(requiringChunkIndex, remainSize, true);
     }
 
     public static RequireNextChunk completed() {
-        return new RequireNextChunk(null, false);
+        return new RequireNextChunk(null, 0L, false);
     }
 }
