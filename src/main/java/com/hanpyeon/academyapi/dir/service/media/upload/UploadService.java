a2653664e64ea76b5cfeaca9db6c4d8731e09efe
huhdy32 <mong3268@gmail.com>
2024-09-03 15:55:11 +0900

    #86 refactor(ChunkHandlerManager): 검증로직 UploadService에서 수행하도록 수정

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/UploadService.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/UploadService.java
@@ -2,10 +2,13 @@ package com.hanpyeon.academyapi.dir.service.media.upload;
 
 import com.hanpyeon.academyapi.dir.dto.RequireNextChunk;
 import com.hanpyeon.academyapi.dir.dto.UploadMediaDto;
-import com.hanpyeon.academyapi.dir.service.media.upload.chunk.ChunkProcessorManager;
+import com.hanpyeon.academyapi.dir.exception.ChunkException;
+import com.hanpyeon.academyapi.dir.service.media.upload.chunk.ChunkHandlerManager;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkFactory;
+import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkGroupInfo;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkedFile;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.storage.ChunkStorage;
+import com.hanpyeon.academyapi.dir.service.media.upload.chunk.validator.ChunkValidateManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,10 +21,22 @@ public class UploadService {
 
     private final ChunkFactory chunkFactory;
     private final @Qualifier(value = "chunkStorage") ChunkStorage chunkStorage;
-    private final ChunkProcessorManager chunkProcessorManager;
+    private final ChunkHandlerManager chunkHandlerManager;
+    private final ChunkValidateManager chunkValidateManager;
 
     public RequireNextChunk upload(final UploadMediaDto uploadMediaDto) {
         final ChunkedFile chunkedFile = chunkFactory.create(uploadMediaDto);
-        return chunkProcessorManager.process(chunkedFile, chunkStorage);
+        try {
+            chunkValidateManager.validate(chunkedFile);
+        } catch (ChunkException exception) {
+            return createRequireInfo(chunkedFile.getChunkGroupInfo(), exception);
+        }
+        return chunkHandlerManager.process(chunkedFile, chunkStorage);
+    }
+
+    private RequireNextChunk createRequireInfo(final ChunkGroupInfo chunkGroupInfo, final ChunkException chunkException) {;
+        final Long remain = chunkGroupInfo.getRequiringChunkSize();
+        final Long nextIndex = chunkGroupInfo.getNextChunkIndex();
+        return RequireNextChunk.need(nextIndex, remain, chunkException.getMessage());
     }
 }
