c4de716bdf755a13bcf8e412e138b6ee9e7ddca5
huhdy32 <mong3268@gmail.com>
2024-09-03 12:23:07 +0900

    #86 refactor(UploadService): ChunkedFile 초기 validation 로직 제거

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/UploadService.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/UploadService.java
@@ -6,7 +6,6 @@ import com.hanpyeon.academyapi.dir.service.media.upload.chunk.ChunkProcessorMana
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkFactory;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkedFile;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.storage.ChunkStorage;
-import com.hanpyeon.academyapi.dir.service.media.upload.chunk.validator.ChunkPreValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,16 +19,9 @@ public class UploadService {
     private final ChunkFactory chunkFactory;
     private final @Qualifier(value = "chunkStorage") ChunkStorage chunkStorage;
     private final ChunkProcessorManager chunkProcessorManager;
-    private final ChunkPreValidator chunkPreValidator;
 
     public RequireNextChunk upload(final UploadMediaDto uploadMediaDto) {
-        final ChunkedFile chunkedFile = getValidatedChunkFile(uploadMediaDto);
-        return chunkProcessorManager.process(chunkedFile, chunkStorage);
-    }
-
-    private ChunkedFile getValidatedChunkFile(final UploadMediaDto uploadMediaDto) {
         final ChunkedFile chunkedFile = chunkFactory.create(uploadMediaDto);
-        chunkPreValidator.preValidate(chunkedFile);
-        return chunkedFile;
+        return chunkProcessorManager.process(chunkedFile, chunkStorage);
     }
 }
