e54969047a8ee5e68f44c000e41a19df526ebbfd
huhdy32 <mong3268@gmail.com>
2024-09-03 15:38:51 +0900

    #86 refactor(ChunkOverSizeValidator): 에러처리 ChunkedFile에서 추출

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/validator/ChunkOverSizeValidator.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/validator/ChunkOverSizeValidator.java
@@ -1,12 +1,17 @@
 package com.hanpyeon.academyapi.dir.service.media.upload.chunk.validator;
 
+import com.hanpyeon.academyapi.dir.exception.ChunkException;
 import com.hanpyeon.academyapi.dir.service.media.upload.chunk.group.ChunkedFile;
+import com.hanpyeon.academyapi.exception.ErrorCode;
 import org.springframework.stereotype.Service;
 
 @Service
 class ChunkOverSizeValidator implements ChunkValidator {
     @Override
     public void validate(ChunkedFile chunkedFile) {
-        chunkedFile.validateCurrSize();
+        final Long remainSize = chunkedFile.getRemainSizeWithThisChunk();
+        if (remainSize < 0) {
+            throw new ChunkException("초과되는 청크 도착 : " + (-remainSize), ErrorCode.CHUNK_SIZE_EXCEPTION);
+        }
     }
 }
