15b9ac68c4e80ed56d00f25c4a15cb56db26793d
huhdy32 <mong3268@gmail.com>
2024-09-04 21:48:29 +0900

    #111 feat(LoadMediasAdapter): src를 활용해 미디어 객체 가져오기 구현

---
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/LoadMediasAdapter.java
@@ -0,0 +1,36 @@
+package com.hanpyeon.academyapi.course.adapter.out;
+
+import com.hanpyeon.academyapi.course.application.port.out.LoadMediasPort;
+import com.hanpyeon.academyapi.course.domain.Media;
+import com.hanpyeon.academyapi.course.domain.MemoMedia;
+import com.hanpyeon.academyapi.exception.ErrorCode;
+import com.hanpyeon.academyapi.media.exception.NoSuchMediaException;
+import com.hanpyeon.academyapi.media.repository.MediaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoadMediasAdapter implements LoadMediasPort {
+
+    private final MediaRepository mediaRepository;
+
+    @Override
+    public List<Media> loadMedias(List<String> mediaSources) {
+        final List<com.hanpyeon.academyapi.media.entity.Media> medias = loadRelatedMedias(mediaSources);
+        return medias.stream().map(media -> Media.createByEntity(media.getMediaName(), media.getSrc(), null))
+                .collect(Collectors.toList());
+    }
+
+    private List<com.hanpyeon.academyapi.media.entity.Media> loadRelatedMedias(final List<String> medias) {
+        final List<com.hanpyeon.academyapi.media.entity.Media> mediaEntities = mediaRepository.findAllBySrcIn(medias);
+        if (medias.size() != mediaEntities.size()) {
+            throw new NoSuchMediaException(ErrorCode.NO_SUCH_MEDIA);
+        }
+        return Collections.unmodifiableList(mediaEntities);
+    }
+}
