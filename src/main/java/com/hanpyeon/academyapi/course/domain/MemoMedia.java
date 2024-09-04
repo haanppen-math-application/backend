99de2eb1b89e0ddc867af34a7182cba742ff0050
huhdy32 <mong3268@gmail.com>
2024-09-04 21:46:52 +0900

    #111 feat(MemoMedia): 메모 도메인에 포함될 참조형식 구성

---
+++ src/main/java/com/hanpyeon/academyapi/course/domain/MemoMedia.java
@@ -0,0 +1,20 @@
+package com.hanpyeon.academyapi.course.domain;
+
+import lombok.Getter;
+
+@Getter
+public class MemoMedia extends Media {
+    private final Integer sequence;
+
+    public MemoMedia(String mediaName, String mediaSource, Long mediaSize, Integer sequence) {
+        super(mediaName, mediaSource, mediaSize);
+        this.sequence = sequence;
+    }
+
+    public static MemoMedia createByEntity(final String mediaName, final String mediaSource, final Long mediaSize, final Integer sequence) {
+        return new MemoMedia(mediaName, mediaSource, mediaSize, sequence);
+    }
+    public static MemoMedia createByMedia(final Media media, final Integer sequence) {
+        return new MemoMedia(media.getMediaName(), media.getMediaSource(), media.getMediaSize(), sequence);
+    }
+}
