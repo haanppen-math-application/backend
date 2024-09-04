0c66b6b4eac66631aa3b1ed4d7da7740dd8d8d34
huhdy32 <mong3268@gmail.com>
2024-09-04 21:39:15 +0900

    #111 feat(UpdateMemoMediaController): 메모에 추가될 영상 자료 순서 업데이트 API

---
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/in/UpdateMemoMediaController.java
@@ -0,0 +1,40 @@
+package com.hanpyeon.academyapi.course.adapter.in;
+
+import com.hanpyeon.academyapi.course.application.dto.UpdateMediaMemoCommand;
+import com.hanpyeon.academyapi.course.application.port.in.AddMediaToMemoUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UpdateMemoMediaController {
+
+    private final AddMediaToMemoUseCase addMediaToMemoUseCase;
+
+    @PutMapping("/api/course/memo/media")
+    @Operation(summary = "메모에 관련된 영상을 업데이트 하는 api", description = "요청 시 순서에 맞게 미디어 파일을 보내야 합니다")
+    @SecurityRequirement(name = "jwtAuth")
+    public ResponseEntity<?> updateMemoMedia(
+            @RequestBody @Valid UpdateMemoMediaController.UpdateMemoMediaRequest updateMemoMediaRequest
+    ) {
+        final UpdateMediaMemoCommand command = updateMemoMediaRequest.toCommand();
+        addMediaToMemoUseCase.addMedia(command);
+        return ResponseEntity.ok().build();
+    }
+
+    record UpdateMemoMediaRequest(
+            @Nonnull Long memoId,
+            @Nonnull List<String> mediaSource
+    ) {
+        UpdateMediaMemoCommand toCommand() {
+            return new UpdateMediaMemoCommand(memoId, mediaSource);
+        }
+    }
+}
