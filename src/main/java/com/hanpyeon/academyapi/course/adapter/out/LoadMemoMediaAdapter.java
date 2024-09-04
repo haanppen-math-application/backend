606f440ec4574e473aa171e058e5e2d752665115
huhdy32 <mong3268@gmail.com>
2024-09-04 21:50:44 +0900

    #111 feat(LoadMemoMediaAdapter): 메모 ID를 활용해 연관된 미디어를 찾아오는 포트 구현

---
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/LoadMemoMediaAdapter.java
@@ -0,0 +1,24 @@
+package com.hanpyeon.academyapi.course.adapter.out;
+
+import com.hanpyeon.academyapi.course.application.port.out.LoadMemoMediaPort;
+import com.hanpyeon.academyapi.course.domain.MemoMedia;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoadMemoMediaAdapter implements LoadMemoMediaPort {
+    private final MemoMediaRepository memoMediaRepository;
+    @Override
+    public List<MemoMedia> loadMedia(Long memoId) {
+        return memoMediaRepository.findAllByMemo_Id(memoId)
+                .stream().map(memoMedia ->  {
+                    final com.hanpyeon.academyapi.media.entity.Media media = memoMedia.getMedia();
+                    return MemoMedia.createByEntity(media.getMediaName(), media.getSrc(), null, memoMedia.getSequence());
+                })
+                .collect(Collectors.toList());
+    }
+}
