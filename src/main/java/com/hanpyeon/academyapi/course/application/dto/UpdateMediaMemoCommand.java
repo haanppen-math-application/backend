8053ae3ef9a5d134a8eeb18b28969737c5485342
huhdy32 <mong3268@gmail.com>
2024-09-04 21:45:43 +0900

    #111 feat(UpdateMediaMemoCommand): 메모 미디어 업데이트 유즈케이스를 위한 Command 객체 구성

---
+++ src/main/java/com/hanpyeon/academyapi/course/application/dto/UpdateMediaMemoCommand.java
@@ -0,0 +1,11 @@
+package com.hanpyeon.academyapi.course.application.dto;
+
+import jakarta.annotation.Nonnull;
+
+import java.util.List;
+
+public record UpdateMediaMemoCommand(
+        @Nonnull Long memoId,
+        @Nonnull List<String> mediaId
+) {
+}
