3c9bcb6dbbe15f334b7d112e28c8dd63c355664c
huhdy32 <mong3268@gmail.com>
2024-09-06 12:00:41 +0900

    #120 feat(DeleteMemoMediaAdapter): 메모 미디어 엔티티 삭제 adapter 구현

--- src/main/java/com/hanpyeon/academyapi/course/adapter/out/DeleteMemoMediaAdapter.java
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/DeleteMemoMediaAdapter.java
@@ -3,6 +3,8 @@ package com.hanpyeon.academyapi.course.adapter.out;
 import com.hanpyeon.academyapi.course.application.port.out.DeleteMemoMediaPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
@@ -13,6 +15,7 @@ class DeleteMemoMediaAdapter implements DeleteMemoMediaPort {
     private final MemoMediaRepository memoMediaRepository;
 
     @Override
+    @Transactional(propagation = Propagation.MANDATORY)
     public void deleteRelatedMedias(Long memoId) {
         if (Objects.isNull(memoId)) {
             throw new IllegalArgumentException("memoID 부재");
