0e3e3fd5a9aa5c1a52805c955c6f6feabb07c4c8
huhdy32 <mong3268@gmail.com>
2024-09-06 20:54:15 +0900

    #122 refactor(ChunkStoreResult): 저장된 파일의 이름을 알 수 있도록 필드 추가

--- src/main/java/com/hanpyeon/academyapi/dir/dto/ChunkStoreResult.java
+++ src/main/java/com/hanpyeon/academyapi/dir/dto/ChunkStoreResult.java
@@ -10,17 +10,17 @@ public class ChunkStoreResult {
     private final Long remainSize;
     private final Boolean needMore;
     private final String errorInformation;
-    private final String storedFilePath;
+    private final String savedName;
     private final String userFileName;
 
-    private ChunkStoreResult(final Boolean isCompleted, final boolean isWrongChunk, final Long nextChunkIndex, final Long remainSize, final Boolean needMore, final String errorInformation, final String storedFilePath, final String userFileName) {
+    private ChunkStoreResult(final Boolean isCompleted, final boolean isWrongChunk, final Long nextChunkIndex, final Long remainSize, final Boolean needMore, final String errorInformation, final String savedFileName, final String userFileName) {
         this.isCompleted = isCompleted;
         this.isWrongChunk = isWrongChunk;
         this.nextChunkIndex = nextChunkIndex;
         this.remainSize = remainSize;
         this.needMore = needMore;
         this.errorInformation = errorInformation;
-        this.storedFilePath = storedFilePath;
+        this.savedName = savedFileName;
         this.userFileName = userFileName;
     }
 
@@ -32,7 +32,7 @@ public class ChunkStoreResult {
         return new ChunkStoreResult(false, false, requiringChunkIndex, remainSize, true, null, null, null);
     }
 
-    public static ChunkStoreResult completed(final String savedPath, final String userFileName) {
-        return new ChunkStoreResult(true, false, null, 0L, false, null, savedPath, userFileName);
+    public static ChunkStoreResult completed(final String savedName, final String userFileName) {
+        return new ChunkStoreResult(true, false, null, 0L, false, null, savedName, userFileName);
     }
 }
