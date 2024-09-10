31dfc92a0ef12b917c93a3b2b98effcac8363e12
huhdy32 <mong3268@gmail.com>
2024-09-10 18:52:55 +0900

    #128 fix(DeleteAttachmentController): 잘못된 매핑 수정

--- src/main/java/com/hanpyeon/academyapi/course/adapter/in/DeleteAttachmentController.java
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/in/DeleteAttachmentController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +16,9 @@ class DeleteAttachmentController {
 
     private final DeleteAttachmentUseCase deleteAttachmentUseCase;
 
-    @DeleteMapping("/api/courses/memos/media/attachment")
+    @DeleteMapping("/api/courses/memos/media/attachment/{targetAttachmentId}")
     public ResponseEntity<?> deleteAttachment(
-            @RequestParam final Long targetAttachmentId,
+            @PathVariable final Long targetAttachmentId,
             @AuthenticationPrincipal final MemberPrincipal memberPrincipal
     ) {
         final DeleteAttachmentCommand command = DeleteAttachmentCommand.of(targetAttachmentId, memberPrincipal.memberId());
