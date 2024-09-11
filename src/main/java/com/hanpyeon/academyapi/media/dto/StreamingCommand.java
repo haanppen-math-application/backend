0482747df9b8684b5d1924a5fcb7315086c6606a
huhdy32 <mong3268@gmail.com>
2024-09-11 19:59:48 +0900

    #139 feat(MediaServiceImpl): 검증 로직 및, 스토리지 접근자 구성

--- src/main/java/com/hanpyeon/academyapi/media/dto/StreamingCommand.java
+++ src/main/java/com/hanpyeon/academyapi/media/dto/StreamingCommand.java
@@ -3,7 +3,10 @@ package com.hanpyeon.academyapi.media.dto;
 
 public interface StreamingCommand {
     String getFileName();
+
     Long getStartRangeIndex(final Long resourceTotalLength);
+
     Long getLastRangeIndex(final Long resourceTotalLength);
+
     Long requestMemberId();
 }
