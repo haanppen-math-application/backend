9a4d1d41d18e9090c7d72cea942b0a48b202b007
huhdy32 <mong3268@gmail.com>
2024-09-13 18:04:52 +0900

    #143 feat: 미디어 조회, 메모-미디어 업데이트 로직 구성

--- src/main/java/com/hanpyeon/academyapi/course/adapter/out/MemoMediaRepository.java
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/out/MemoMediaRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 interface MemoMediaRepository extends JpaRepository<MemoMedia, Long> {
     void deleteMemoMediaByMemo_Id(final Long memoId);
     List<MemoMedia> findAllByMemo_Id(final Long memoId);
+    Optional<MemoMedia> findMemoMediaByIdAndMemo_Id(final Long memoMediaId, final Long memoId);
+    Optional<MemoMedia> findMemoMediaByMedia_SrcAndMemo_Id(final String mediaSrc, final Long memoId);
 }
