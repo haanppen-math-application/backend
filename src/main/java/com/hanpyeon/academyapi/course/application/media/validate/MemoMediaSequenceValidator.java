ae5e31683054992e49410021c5ed0bcb1c6a7630
huhdy32 <mong3268@gmail.com>
2024-09-13 18:11:17 +0900

    #143 feat(MemoMEdiaSequenceValidator): 시퀀스 validation 로직 구성

---
+++ src/main/java/com/hanpyeon/academyapi/course/application/media/validate/MemoMediaSequenceValidator.java
@@ -0,0 +1,29 @@
+package com.hanpyeon.academyapi.course.application.media.validate;
+
+import com.hanpyeon.academyapi.course.application.exception.MemoMediaException;
+import com.hanpyeon.academyapi.course.domain.MemoMedia;
+import com.hanpyeon.academyapi.course.domain.MemoMediaContainer;
+import com.hanpyeon.academyapi.exception.ErrorCode;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemoMediaSequenceValidator implements MemoMediaContainerValidator {
+
+    @Override
+    public void validate(MemoMediaContainer container) {
+        final List<MemoMedia> memoMedias = container.getMemoMedias();
+        for (int requireSequence = 0; requireSequence < memoMedias.size(); requireSequence++) {
+            boolean currSequenceFounded = false;
+            for (final MemoMedia memoMedia : memoMedias) {
+                if (memoMedia.getSequence().intValue() == requireSequence) {
+                    currSequenceFounded = true;
+                }
+            }
+            if (currSequenceFounded == false) {
+                throw new MemoMediaException("해당 시퀀스 부재 : " + requireSequence, ErrorCode.MEMO_MEDIA_SEQUENCE);
+            }
+        }
+    }
+}
