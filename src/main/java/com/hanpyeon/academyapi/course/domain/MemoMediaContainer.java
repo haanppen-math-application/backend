6c20cfbc0770517a0cf491e42ec0a62cdcb1b937
huhdy32 <mong3268@gmail.com>
2024-09-13 18:06:04 +0900

    #143 feat(MemoMediaContainer): getter 추가

--- src/main/java/com/hanpyeon/academyapi/course/domain/MemoMediaContainer.java
+++ src/main/java/com/hanpyeon/academyapi/course/domain/MemoMediaContainer.java
@@ -3,12 +3,21 @@ package com.hanpyeon.academyapi.course.domain;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemoMediaContainer {
     private final List<MemoMedia> memoMedias;
     private final Long memoId;
+
+    public Long getMemoId() {
+        return this.memoId;
+    }
+
+    public List<MemoMedia> getMemoMedias() {
+        return Collections.unmodifiableList(this.memoMedias);
+    }
 
 
     public static MemoMediaContainer of(final List<MemoMedia> memoMedias, final Long memoId) {
