68fb18bafa8528b858af6b354dd0d90d57287b44
huhdy32 <mong3268@gmail.com>
2024-09-19 10:03:00 +0900

    #145 feat(RegisterMemoMediaController): 메모에 미디어를 추가하는 API 구성

---
+++ src/main/java/com/hanpyeon/academyapi/course/adapter/in/RegisterMemoMediaController.java
@@ -0,0 +1,40 @@
+package com.hanpyeon.academyapi.course.adapter.in;
+
+import com.hanpyeon.academyapi.course.application.dto.RegisterMemoMediaCommand;
+import com.hanpyeon.academyapi.course.application.port.in.RegisterMemoMediaUseCase;
+import com.hanpyeon.academyapi.security.authentication.MemberPrincipal;
+import jakarta.annotation.Nonnull;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RegisterMemoMediaController {
+
+    private final RegisterMemoMediaUseCase registerMemoMediaUseCase;
+
+    @PostMapping("/api/cours/memo/media")
+    public ResponseEntity<?> addMemoMedia(
+            @RequestBody @Valid final RegisterMemoMediaRequest request,
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal
+    ) {
+        final RegisterMemoMediaCommand command = request.toCommand(memberPrincipal.memberId());
+        registerMemoMediaUseCase.register(command);
+        return ResponseEntity.created(null).build();
+    }
+
+
+    record RegisterMemoMediaRequest(
+            @Nonnull String mediaSource,
+            @Nonnull Long memoId
+    ) {
+        RegisterMemoMediaCommand toCommand(final Long requestMemberId) {
+            return new RegisterMemoMediaCommand(mediaSource, memoId, requestMemberId);
+        }
+    }
+}
