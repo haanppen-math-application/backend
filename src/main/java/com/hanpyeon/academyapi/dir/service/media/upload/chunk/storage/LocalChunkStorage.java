330e5e19b5582800ca998113c8fef9a3cb1e9d16
huhdy32 <mong3268@gmail.com>
2024-09-22 21:29:13 +0900

    #hotfix(LocalChunkStorage): Comparator<Path> 파일이름 역순 정렬

--- src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/storage/LocalChunkStorage.java
+++ src/main/java/com/hanpyeon/academyapi/dir/service/media/upload/chunk/storage/LocalChunkStorage.java
@@ -62,7 +62,7 @@ class LocalChunkStorage extends LocalStorage implements ChunkStorage {
 
     private List<InputStream> loadSequentialFiles(final ChunkGroupInfo chunkGroupInfo) {
         return loadPaths(chunkGroupInfo)
-                .sorted((path1, path2) -> String.valueOf(path1.getFileName()).compareTo(String.valueOf(path2)))
+                .sorted((path1, path2) -> String.valueOf(path2.getFileName()).compareTo(String.valueOf(path1.getFileName())))
                 .map(path -> {
                     try {
                         return Files.newInputStream(path, StandardOpenOption.READ);
