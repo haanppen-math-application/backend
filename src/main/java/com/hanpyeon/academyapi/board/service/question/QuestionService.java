697de41fd80c3a5a513617b4920d164835969fcc
huhdy32 <mong3268@gmail.com>
2024-10-06 17:58:01 +0900

    #183 feat: 페이징 기반 질문 조회 구현

--- src/main/java/com/hanpyeon/academyapi/board/service/question/QuestionService.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/QuestionService.java
@@ -1,23 +1,23 @@
 package com.hanpyeon.academyapi.board.service.question;
 
 import com.hanpyeon.academyapi.aspect.log.WarnLoggable;
+import com.hanpyeon.academyapi.board.dao.QuestionRepository;
 import com.hanpyeon.academyapi.board.dto.*;
 import com.hanpyeon.academyapi.board.entity.Question;
 import com.hanpyeon.academyapi.board.exception.NoSuchQuestionException;
-import com.hanpyeon.academyapi.board.dao.QuestionRepository;
+import com.hanpyeon.academyapi.board.mapper.BoardMapper;
 import com.hanpyeon.academyapi.board.service.question.access.QuestionAccessManager;
 import com.hanpyeon.academyapi.board.service.question.delete.QuestionDeleteManager;
 import com.hanpyeon.academyapi.board.service.question.register.QuestionRegisterManger;
 import com.hanpyeon.academyapi.board.service.question.update.QuestionUpdateManager;
-import com.hanpyeon.academyapi.paging.CursorResponse;
 import com.hanpyeon.academyapi.exception.ErrorCode;
+import com.hanpyeon.academyapi.paging.PagedResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-
-import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -27,6 +27,7 @@ public class QuestionService {
     private final QuestionUpdateManager questionUpdateManager;
     private final QuestionDeleteManager questionDeleteManager;
     private final QuestionAccessManager questionAccessManager;
+    private final BoardMapper boardMapper;
 
     private final QuestionRepository questionRepository;
 
@@ -43,24 +44,16 @@ public class QuestionService {
         return questionAccessManager.getSingle(question);
     }
 
-    public CursorResponse<QuestionPreview> loadQuestionsByCursor(final Long cursorIndex, final Pageable pageable) {
-        final List<Question> questions = questionRepository.findQuestionsByIdIsGreaterThanEqual(cursorIndex, pageable);
-        return new CursorResponse<>(questionAccessManager.mapToPreview(questions), getNextCursor(questions, pageable.getPageSize()));
+    @Transactional(readOnly = true)
+    public PagedResponse<QuestionPreview> loadQuestionsByOffset(final Pageable pageable) {
+        final Page<Question> questions = questionRepository.findAllBy(pageable);
+        return PagedResponse.of(questions.map(question -> boardMapper.createQuestionPreview(question)));
     }
 
-    public CursorResponse<QuestionPreview> loadMyQuestionsByCursor(final Long cursorIndex, final Long memberId, final Pageable pageable) {
-        final List<Question> questions = questionRepository.findQuestionsByIdIsGreaterThanEqualAndAndOwnerMemberId(cursorIndex, memberId, pageable);
-        return new CursorResponse<>(questionAccessManager.mapToPreview(questions), getNextCursor(questions, pageable.getPageSize()));
-    }
-
-    private Long getNextCursor(final List<Question> questions, final Integer pageSize) {
-        Long nextCursorIndex = null;
-        if (questions.size() < pageSize) {
-            nextCursorIndex = null;
-        }else {
-            nextCursorIndex = questions.get(questions.size() - 1).getId() + 1;
-        }
-        return nextCursorIndex;
+    @Transactional(readOnly = true)
+    public PagedResponse<QuestionPreview> loadMyQuestionsByOffset(final Long memberId, final Pageable pageable) {
+        final Page<Question> questions = questionRepository.findQuestionsByOwnerMemberId(memberId, pageable);
+        return PagedResponse.of(questions.map(question -> boardMapper.createQuestionPreview(question)));
     }
 
     @Transactional
