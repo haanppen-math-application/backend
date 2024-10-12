f7b5a22181064c4b48afa224881de4600f32f2cb
huhdy32 <mong3268@gmail.com>
2024-10-13 00:16:28 +0900

    #197 fix: 질문 제목 검증에서 content 검증하던 에러 수정

--- src/main/java/com/hanpyeon/academyapi/board/service/question/validate/QuestionTitleContentValidator.java
+++ src/main/java/com/hanpyeon/academyapi/board/service/question/validate/QuestionTitleContentValidator.java
@@ -2,7 +2,6 @@ package com.hanpyeon.academyapi.board.service.question.validate;
 
 import com.hanpyeon.academyapi.board.entity.Question;
 import com.hanpyeon.academyapi.board.exception.BoardException;
-import com.hanpyeon.academyapi.board.exception.QuestionContentOverSizeException;
 import com.hanpyeon.academyapi.exception.ErrorCode;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +11,8 @@ import java.util.Objects;
 class QuestionTitleContentValidator implements QuestionValidator {
     @Override
     public void validate(Question question) {
-        if (Objects.isNull(question.getContent()) || question.getContent().isBlank()) {
+        if (Objects.isNull(question.getTitle()) || question.getTitle().isBlank()) {
             throw new BoardException("현재 질문에는 제목이 포함되야 합니다", ErrorCode.ILLEGAL_QUESTION_EXCEPTION);
-        }
-        if (question.getContent().length() > 500) {
-            throw new QuestionContentOverSizeException("500 이하 여야 합니다", ErrorCode.QUESTION_CONTENT_OVERSIZE);
         }
     }
 }
