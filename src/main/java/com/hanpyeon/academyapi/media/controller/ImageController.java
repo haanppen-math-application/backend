2cc3f60fda65b02c614ae4853e56d67a05781a2a
huhdy32 <mong3268@gmail.com>
2024-11-23 21:04:19 +0900

    feat(ImageController): 이미지 단일 저장 로직 구현

--- src/main/java/com/hanpyeon/academyapi/media/controller/ImageController.java
+++ src/main/java/com/hanpyeon/academyapi/media/controller/ImageController.java
@@ -1,19 +1,22 @@
 package com.hanpyeon.academyapi.media.controller;
 
+import com.hanpyeon.academyapi.media.dto.ImageUrlDto;
 import com.hanpyeon.academyapi.media.dto.MediaDto;
 import com.hanpyeon.academyapi.media.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.io.IOException;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
@@ -30,5 +33,13 @@ public class ImageController {
         return ResponseEntity.ok()
                 .contentType(media.mediaType())
                 .body(media.data().readAllBytes());
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<?> postImage(
+            @RequestPart(value = "image", required = true) final MultipartFile image
+    ) {
+        final ImageUrlDto imageUrlDto = imageService.saveSingleImage(image);
+        return ResponseEntity.ok(imageUrlDto);
     }
 }
