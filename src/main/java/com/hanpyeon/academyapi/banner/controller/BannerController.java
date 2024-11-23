42dd7adccfdb668e97b06a86533be8d06eb3a743
huhdy32 <mong3268@gmail.com>
2024-11-23 19:08:50 +0900

    #209 feat(Banner): 배너 수정 기능 추가

--- src/main/java/com/hanpyeon/academyapi/banner/controller/BannerController.java
+++ src/main/java/com/hanpyeon/academyapi/banner/controller/BannerController.java
@@ -2,9 +2,11 @@ package com.hanpyeon.academyapi.banner.controller;
 
 import com.hanpyeon.academyapi.banner.BannerService;
 import com.hanpyeon.academyapi.banner.dto.AddBannerCommand;
+import com.hanpyeon.academyapi.banner.dto.ChangeBannerCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +25,24 @@ class BannerController {
         return ResponseEntity.ok().build();
     }
 
+    @PutMapping("/api/banner")
+    public ResponseEntity<?> modifyBanner(
+            @RequestBody ChangeBannerRequest changeBannerRequest
+    ) {
+        final ChangeBannerCommand changeBannerCommand = new ChangeBannerCommand(
+                changeBannerRequest.bannerId(),
+                changeBannerRequest.content()
+        );
+        bannerService.changeBanner(changeBannerCommand);
+        return ResponseEntity.ok().build();
+    }
+
     record AddBannerRequest(
+            String content
+    ) {
+    }
+    record ChangeBannerRequest(
+            Long bannerId,
             String content
     ) {
     }
