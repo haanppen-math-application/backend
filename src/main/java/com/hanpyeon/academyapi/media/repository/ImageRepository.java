49e478679a4aa8c67da2b8906f483843504b4ed4
huhdy32 <mong3268@gmail.com>
2024-11-28 11:48:29 +0900

    #212 feat(CommentController): 댓글 저장 방식 이미지 url 로 하도록 수정

--- src/main/java/com/hanpyeon/academyapi/media/repository/ImageRepository.java
+++ src/main/java/com/hanpyeon/academyapi/media/repository/ImageRepository.java
@@ -1,10 +1,13 @@
 package com.hanpyeon.academyapi.media.repository;
 
 import com.hanpyeon.academyapi.media.entity.Image;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Boolean existsBySrc(final String src);
+    Boolean existsAllBySrcIn(final List<String> src);
+    List<Image> findAllBySrcIn(final List<String> src);
 }
