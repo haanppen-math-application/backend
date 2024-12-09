40ea1c4238a3b84c4c297255d8e2f8612aa18b85
huhdy32 <mong3268@gmail.com>
2024-12-09 15:15:37 +0900

    #228 test: JPA 테스트 코드 수정

--- src/main/java/com/hanpyeon/academyapi/online/service/course/MemberLoader.java
+++ src/main/java/com/hanpyeon/academyapi/online/service/course/MemberLoader.java
@@ -9,21 +9,25 @@ import com.hanpyeon.academyapi.security.Role;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberLoader {
     private final MemberRepository memberRepository;
 
+    @Transactional(propagation = Propagation.MANDATORY, readOnly = true)
     public Member findTeacher(final Long memberId) {
         return memberRepository.findMemberByIdAndRoleAndRemovedIsFalse(memberId, Role.TEACHER)
                 .orElseThrow(() -> new NoSuchMemberException(ErrorCode.NO_SUCH_MEMBER));
     }
 
+    @Transactional(propagation = Propagation.MANDATORY, readOnly = true)
     public List<Member> findStudents(final List<Long> studentIds) {
         final List<Member> students = memberRepository.findMembersByIdIsInAndRoleAndRemovedIsFalse(studentIds, Role.STUDENT);
         if (students.size() != studentIds.size()) {
-            throw new BusinessException(ErrorCode.CANNOT_FIND_USER);
+            throw new BusinessException("학생을 찾지 못함. 찾은 학생 수" + students.size() + " 요청 수 : " + studentIds.size()  , ErrorCode.CANNOT_FIND_USER);
         }
         return students;
     }
--- src/test/java/com/hanpyeon/academyapi/online/service/course/OnlineCourseRegisterServiceTest.java
+++ src/test/java/com/hanpyeon/academyapi/online/service/course/OnlineCourseRegisterServiceTest.java
@@ -7,6 +7,7 @@ import com.hanpyeon.academyapi.online.dao.OnlineStudentRepository;
 import com.hanpyeon.academyapi.online.dto.AddOnlineCourseCommand;
 import com.hanpyeon.academyapi.online.service.course.OnlineCourseRegisterService;
 import com.hanpyeon.academyapi.security.Role;
+import jakarta.persistence.EntityManager;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 class OnlineCourseRegisterServiceTest {
 
     @Autowired
+    private EntityManager entityManager;
+    @Autowired
     private OnlineCourseRepository onlineCourseRepository;
     @Autowired
     private MemberRepository memberRepository;
@@ -31,34 +34,46 @@ class OnlineCourseRegisterServiceTest {
     @Test
     @Transactional
     void 온라인_강의_등록_테스트() {
-        this.memberRepository.saveAll(
-                List.of(
-                        Member.builder()
-                                .name("test")
-                                .encryptedPassword("test")
-                                .role(Role.TEACHER)
-                                .build(),
-                        Member.builder()
-                                .name("test")
-                                .encryptedPassword("test")
-                                .role(Role.STUDENT)
-                                .build(),
-                        Member.builder()
-                                .name("test")
-                                .encryptedPassword("test")
-                                .role(Role.STUDENT)
-                                .build()
-                )
+        final List<Member> members = List.of(
+                Member.builder()
+                        .name("test")
+                        .encryptedPassword("test")
+                        .role(Role.TEACHER)
+                        .build(),
+                Member.builder()
+                        .name("test")
+                        .encryptedPassword("test")
+                        .role(Role.STUDENT)
+                        .build(),
+                Member.builder()
+                        .name("test")
+                        .encryptedPassword("test")
+                        .role(Role.STUDENT)
+                        .build()
         );
+        this.memberRepository.saveAll(members);
+        entityManager.flush();
+        entityManager.clear();
+
+        final Long requestMemberId = members.get(0).getId();
+        final Long target1 = members.get(1).getId();
+        final Long target2 = members.get(2).getId();
+
+        entityManager.flush();
+        entityManager.clear();
+
         final AddOnlineCourseCommand addOnlineCourseCommand = new AddOnlineCourseCommand(
-                1L,
+                requestMemberId,
                 Role.TEACHER,
                 "test",
-                List.of(2L, 3L),
-                1L
+                List.of(target1, target2),
+                requestMemberId
         );
 
         onlineCourseService.addOnlineCourse(addOnlineCourseCommand);
+
+        entityManager.flush();
+        entityManager.clear();
 
         org.junit.jupiter.api.Assertions.assertAll(() -> {
             Assertions.assertThat(onlineCourseRepository.findAll().size()).isEqualTo(1);
