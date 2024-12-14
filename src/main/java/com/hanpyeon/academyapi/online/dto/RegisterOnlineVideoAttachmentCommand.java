46e6b46c194bdeca4c6123a23449c1e31982a2b0
huhdy32 <mong3268@gmail.com>
2024-12-14 18:05:29 +0900

    feat: 첨부내용에 제목 기능 추가

--- src/main/java/com/hanpyeon/academyapi/online/dto/RegisterOnlineVideoAttachmentCommand.java
+++ src/main/java/com/hanpyeon/academyapi/online/dto/RegisterOnlineVideoAttachmentCommand.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterOnlineVideoAttachmentCommand(
+        @NotBlank String attachmentTitle,
         @NotBlank String attachmentContent,
         @NotNull Long onlineCourseId,
         @NotNull Long onlineVideoId,
