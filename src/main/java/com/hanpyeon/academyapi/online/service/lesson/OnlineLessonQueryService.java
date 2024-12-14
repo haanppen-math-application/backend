b38c629af75b3d36b4ebad9cdda78e6e61ece906
huhdy32 <mong3268@gmail.com>
2024-12-14 14:09:37 +0900

    feat(OnlineLessonQueryService): 온라인 수업 상세 조회에 mediaSrc 추가

--- src/main/java/com/hanpyeon/academyapi/online/service/lesson/OnlineLessonQueryService.java
+++ src/main/java/com/hanpyeon/academyapi/online/service/lesson/OnlineLessonQueryService.java
@@ -5,10 +5,12 @@ import com.hanpyeon.academyapi.exception.ErrorCode;
 import com.hanpyeon.academyapi.online.dao.OnlineCategory;
 import com.hanpyeon.academyapi.online.dao.OnlineCourse;
 import com.hanpyeon.academyapi.online.dao.OnlineCourseRepository;
+import com.hanpyeon.academyapi.online.dao.OnlineStudent;
 import com.hanpyeon.academyapi.online.dao.OnlineVideo;
 import com.hanpyeon.academyapi.online.dao.OnlineVideoAttachment;
 import com.hanpyeon.academyapi.online.dto.LessonCategoryInfo;
 import com.hanpyeon.academyapi.online.dto.OnlineLessonDetail;
+import com.hanpyeon.academyapi.online.dto.OnlineLessonQueryCommand;
 import com.hanpyeon.academyapi.online.dto.OnlineVideoAttachmentDetail;
 import com.hanpyeon.academyapi.online.dto.OnlineVideoDetail;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @Service
 @RequiredArgsConstructor
@@ -24,24 +27,34 @@ import org.springframework.transaction.annotation.Transactional;
 public class OnlineLessonQueryService {
     private final OnlineCourseRepository onlineCourseRepository;
 
-    public OnlineLessonDetail loadDetails(final Long courseId) {
-        final OnlineCourse onlineCourses = onlineCourseRepository.loadOnlineCourseAndVideosAndCategoryById(courseId)
-                .orElseThrow(() -> new BusinessException(courseId + "를 찾을 수 없음", ErrorCode.ONLINE_COURSE_EXCEPTION));
-        return mapToDetail(onlineCourses);
+    public OnlineLessonDetail loadDetails(@Validated final OnlineLessonQueryCommand command) {
+        final OnlineCourse onlineCourses = onlineCourseRepository.loadOnlineCourseAndVideosAndCategoryById(
+                        command.courseId())
+                .orElseThrow(() -> new BusinessException(command.courseId() + "를 찾을 수 없음",
+                        ErrorCode.ONLINE_COURSE_EXCEPTION));
+        return mapToDetail(onlineCourses, command.requestMemberId());
     }
 
-    private OnlineLessonDetail mapToDetail(final OnlineCourse onlineCourse) {
+    private OnlineLessonDetail mapToDetail(final OnlineCourse onlineCourse, final Long requestMemberId) {
+        final boolean isIncludedStudent = checkIncludedStudent(requestMemberId, onlineCourse.getOnlineStudents());
         return new OnlineLessonDetail(
                 onlineCourse.getId(),
                 onlineCourse.getCourseTitle(),
                 onlineCourse.getCourseRange(),
                 onlineCourse.getCourseContent(),
                 onlineCourse.getVideos().stream()
-                        .map(this::mapToDetail)
+                        .map(onlineVideo -> this.mapToDetail(onlineVideo, isIncludedStudent))
                         .collect(Collectors.toList()),
                 toCategory(onlineCourse.getOnlineCategory())
         );
     }
+
+    private boolean checkIncludedStudent(final Long requestMemberId, final List<OnlineStudent> studentIds) {
+        return studentIds.stream()
+                .map(onlineStudent -> onlineStudent.getMember().getId())
+                .anyMatch(studentId -> studentId.equals(requestMemberId));
+    }
+
     private LessonCategoryInfo toCategory(final OnlineCategory onlineCategory) {
         if (Objects.isNull(onlineCategory)) {
             return null;
@@ -50,11 +63,26 @@ public class OnlineLessonQueryService {
                 onlineCategory.getCategoryName());
     }
 
-    private OnlineVideoDetail mapToDetail(final OnlineVideo onlineVideo) {
-        return new OnlineVideoDetail(onlineVideo.getId(), onlineVideo.getVideoSequence(), onlineVideo.getVideoName(),
-                onlineVideo.getVideoAttachments().stream()
-                        .map(this::mapToDetail)
-                        .toList()
+    private OnlineVideoDetail mapToDetail(final OnlineVideo onlineVideo, final boolean isIncludedMember) {
+        if (onlineVideo.getPreview() || isIncludedMember) {
+            return new OnlineVideoDetail(
+                    onlineVideo.getId(),
+                    onlineVideo.getVideoSequence(),
+                    onlineVideo.getVideoName(),
+                    onlineVideo.getPreview(),
+                    onlineVideo.getMedia().getSrc(),
+                    onlineVideo.getVideoAttachments().stream()
+                            .map(this::mapToDetail)
+                            .toList()
+            );
+        }
+        return new OnlineVideoDetail(
+                onlineVideo.getId(),
+                onlineVideo.getVideoSequence(),
+                onlineVideo.getVideoName(),
+                onlineVideo.getPreview(),
+                null,
+                null
         );
     }
 
