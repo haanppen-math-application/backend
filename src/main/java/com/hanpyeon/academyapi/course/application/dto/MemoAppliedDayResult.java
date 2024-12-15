bd71832e76e55baf05dddc24e02a3f46202ca113
huhdy32 <mong3268@gmail.com>
2024-12-15 15:19:31 +0900

    feat: 월별 반 조회 기능 수정

--- src/main/java/com/hanpyeon/academyapi/course/application/dto/MemoAppliedDayResult.java
+++ src/main/java/com/hanpyeon/academyapi/course/application/dto/MemoAppliedDayResult.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public record MemoAppliedDayResult(
         Long courseId,
+        String courseName,
         Long courseMemoId,
         LocalDateTime registeredDateTime
 ) {
