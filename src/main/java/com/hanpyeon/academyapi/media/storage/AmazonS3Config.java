6ea2fc9fa76855f99908b2ca9f6cf7f6acddc90c
huhdy32 <mong3268@gmail.com>
2025-01-15 18:00:41 +0900

    hotfix: aws s3 구성 클래스 추가

--- src/main/java/com/hanpyeon/academyapi/media/storage/AmazonS3Config.java
+++ src/main/java/com/hanpyeon/academyapi/media/storage/AmazonS3Config.java
@@ -5,8 +5,10 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-//@Configuration
+@Configuration
+@Profile("deploy")
 class AmazonS3Config {
     private final String accessKey;
     private final String secretKey;
