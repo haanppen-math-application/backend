0fa971d21f7b0b5c1794906f5c78de450b221591
huhdy32 <mong3268@gmail.com>
2025-02-11 11:13:44 +0900

    feat: directory 이름에 공백, 숫자 포함 가능하도록 변경

--- src/main/java/com/hanpyeon/academyapi/dir/dto/CreateDirectoryDto.java
+++ src/main/java/com/hanpyeon/academyapi/dir/dto/CreateDirectoryDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Pattern;
 import org.springframework.lang.NonNull;
 
 public record CreateDirectoryDto(
-        @NotBlank @Pattern(regexp = "^[가-힣a-zA-Z]+$") String directoryName,
+        @NotBlank @Pattern(regexp = "^[가-힣a-zA-Z0-9 ]+$") String directoryName,
         @NotBlank @Pattern(regexp = "^/+&") String parentDirPath,
         @NonNull Long ownerId,
         Boolean canViewByEveryone,
