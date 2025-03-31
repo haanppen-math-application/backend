782b856561995e18c1b5d978a9a412f26efec295
huhdy32 <mong3268@gmail.com>
2025-03-31 23:51:45 +0900

    fix: stacktrace 로그 노출 제거

--- src/main/java/com/hanpyeon/academyapi/exception/ApiExceptionHandler.java
+++ src/main/java/com/hanpyeon/academyapi/exception/ApiExceptionHandler.java
@@ -52,9 +52,9 @@ public class ApiExceptionHandler {
     public ResponseEntity<ExceptionResponseBody> businessExceptionHandler(final BusinessException businessException) {
         ErrorCode errorCode = businessException.getErrorCode();
         String details = businessException.getMessage();
-
-        logger.debug("[ EXCEPTION WITH ] -> {} : {}", errorCode, details);
-        logger.debug("[ STACK TRACE ] -> {} ", Arrays.toString(businessException.getStackTrace()));
+//
+//        logger.debug("[ EXCEPTION WITH ] -> {} : {}", errorCode, details);
+//        logger.debug("[ STACK TRACE ] -> {} ", Arrays.toString(businessException.getStackTrace()));
         return createExceptionResponse(errorCode, ExceptionResponseBody.of(errorCode, details));
     }
 
