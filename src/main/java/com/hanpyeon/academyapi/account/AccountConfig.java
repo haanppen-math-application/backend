8f65bffff9a5113ada4fa57ef1b3f28afa9ec9b6
huhdy32 <mong3268@gmail.com>
2025-04-11 15:19:40 +0900

    refactor: account configuration 빈 등록 로직 집중화

--- src/main/java/com/hanpyeon/academyapi/account/AccountConfig.java
+++ src/main/java/com/hanpyeon/academyapi/account/AccountConfig.java
@@ -1,5 +1,6 @@
 package com.hanpyeon.academyapi.account;
 
+import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,15 @@ public class AccountConfig {
     @Bean
     Random random() {
         return new Random();
+    }
+
+    @Bean
+    TimeProvider timeProvider() {
+        return new TimeProvider();
+    }
+
+    @Bean
+    AccountScheduler accountScheduler(final MemberRepository memberRepository) {
+        return new AccountScheduler(memberRepository);
     }
 }
--- src/main/java/com/hanpyeon/academyapi/account/AccountScheduler.java
+++ src/main/java/com/hanpyeon/academyapi/account/AccountScheduler.java
@@ -2,18 +2,22 @@ package com.hanpyeon.academyapi.account;
 
 import com.hanpyeon.academyapi.account.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
 @RequiredArgsConstructor
+@Slf4j
 public class AccountScheduler {
     private final MemberRepository memberRepository;
 
     @Scheduled(cron = "0 0 0 * * *")
     @Transactional
     public void resetVerificationStatus() {
+        log.info("Reset verification status for members : Started");
         memberRepository.resetVerificationInfos();
+        log.info("Reset verification status for members : Finished");
     }
 }
