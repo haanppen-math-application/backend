d26b42586f30844596a36f13ea9faf22914998a0
huhdy32 <mong3268@gmail.com>
2025-04-11 18:24:08 +0900

    refactor: log 에 verification 초기화 소요 시간 기록

--- src/main/java/com/hanpyeon/academyapi/account/AccountScheduler.java
+++ src/main/java/com/hanpyeon/academyapi/account/AccountScheduler.java
@@ -1,10 +1,12 @@
 package com.hanpyeon.academyapi.account;
 
 import com.hanpyeon.academyapi.account.repository.MemberRepository;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -15,7 +17,12 @@ public class AccountScheduler {
     @Transactional
     public void resetVerificationStatus() {
         log.info("Reset verification status for members : Started");
+
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         memberRepository.resetVerificationInfos();
-        log.info("Reset verification status for members : Finished");
+        final long totalTimeMillis = stopWatch.getTotalTimeMillis();
+
+        log.info("Reset verification status for members : Finished with [{}.{}] seconds", totalTimeMillis / 1000, totalTimeMillis % 1000);
     }
 }
