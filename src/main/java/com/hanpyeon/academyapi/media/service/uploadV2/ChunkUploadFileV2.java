94d841a8c67c026263a6eb6408a374aad54fe8e9
huhdy32 <mong3268@gmail.com>
2025-04-16 14:51:40 +0900

    feat: 청크 스토리지 구현

---
+++ src/main/java/com/hanpyeon/academyapi/media/service/uploadV2/ChunkUploadFileV2.java
@@ -0,0 +1,37 @@
+package com.hanpyeon.academyapi.media.service.uploadV2;
+
+import com.hanpyeon.academyapi.media.storage.uploadfile.UploadFile;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ChunkUploadFileV2 implements UploadFile {
+    private final String uniqueId;
+    private final InputStream inputStream;
+    private final int partNumber;
+
+    public ChunkUploadFileV2(String uniqueId, MultipartFile multipartFile, int partNumber) {
+        this.uniqueId = uniqueId;
+        try {
+            this.inputStream = multipartFile.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.partNumber = partNumber;
+    }
+
+    @Override
+    public String getUniqueFileName() {
+        return uniqueId + "_" + partNumber;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    @Override
+    public String getExtension() {
+        return "";
+    }
+}
---
+++ src/main/java/com/hanpyeon/academyapi/media/service/uploadV2/CombinedFile.java
@@ -0,0 +1,27 @@
+package com.hanpyeon.academyapi.media.service.uploadV2;
+
+import com.hanpyeon.academyapi.media.storage.uploadfile.UploadFile;
+import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CombinedFile implements UploadFile {
+    private final InputStream inputStream;
+    private final String uniqueId;
+    private final String extension;
+
+    @Override
+    public String getUniqueFileName() {
+        return uniqueId;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    @Override
+    public String getExtension() {
+        return extension;
+    }
+}
