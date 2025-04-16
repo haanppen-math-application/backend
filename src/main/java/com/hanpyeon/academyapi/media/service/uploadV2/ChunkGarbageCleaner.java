6e2a661f6ded10cd4cf5b29f6430f90e54d6597e
huhdy32 <mong3268@gmail.com>
2025-04-16 14:50:16 +0900

    feat: 업로드 되지 않는 파일집합 자동 삭제로직 구현

---
+++ src/main/java/com/hanpyeon/academyapi/media/service/uploadV2/ChunkGarbageCleaner.java
@@ -0,0 +1,30 @@
+package com.hanpyeon.academyapi.media.service.uploadV2;
+
+import com.hanpyeon.academyapi.account.TimeProvider;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class ChunkGarbageCleaner {
+    private final ChunkPartNumberManager chunkNumberManager;
+    private final ChunkStorageV2 chunkStorageV2;
+    private final UploadIdLifeCycleManager uniqueIdManger;
+    private final TimeProvider timeProvider;
+
+    @Scheduled(initialDelay = 10_000, fixedDelay = 1800_000)
+    public void removeRemained() {
+        log.info("started : Removing remained chunks");
+        final List<String> removable = uniqueIdManger.getExpiredIds(timeProvider.getCurrentTime());
+        removable.forEach(uniqueId -> {
+            uniqueIdManger.remove(uniqueId);
+            chunkNumberManager.remove(uniqueId);
+            chunkStorageV2.removeRelated(uniqueId);
+        });
+        log.info("finished : Removed {} remained chunks", removable.size());
+    }
+}
