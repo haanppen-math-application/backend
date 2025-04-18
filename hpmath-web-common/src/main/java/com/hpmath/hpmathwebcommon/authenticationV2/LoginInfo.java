1bac5f749c3aea81e3daa249812a5515b6fe60d5
huhdy32 <mong3268@gmail.com>
2025-04-18 17:01:19 +0900

    docs: 인증 정보 확인 문서 설명 추가

--- hpmath-web-common/src/main/java/com/hpmath/hpmathwebcommon/authenticationV2/LoginInfo.java
+++ hpmath-web-common/src/main/java/com/hpmath/hpmathwebcommon/authenticationV2/LoginInfo.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * {@link com.hpmath.hpmathwebcommon.authentication.MemberPrincipal} 을 리턴하도록 하는 어노테이션입니다.
+ * {@link LoginIdMethodArgumentResolver} 를 통해 실행됩니다.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface LoginInfo {
