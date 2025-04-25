843e3a44ed10188733b2a17afce2d0ed9211f25c
huhdy32 <mong3268@gmail.com>
2025-04-25 19:19:37 +0900

    fix: 역직렬화 오류 수정

--- hpmath-core-api/src/main/java/com/hpmath/hpmathcoreapi/account/dto/Password.java
+++ hpmath-core-api/src/main/java/com/hpmath/hpmathcoreapi/account/dto/Password.java
@@ -1,15 +1,31 @@
 package com.hpmath.hpmathcoreapi.account.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.hpmath.hpmathcoreapi.account.dto.Password.PasswordDeserializer;
+import com.hpmath.hpmathcoreapi.account.dto.Password.PasswordSerializer;
 import com.hpmath.hpmathcoreapi.account.validation.PasswordConstraint;
 import com.hpmath.hpmathwebcommon.PasswordHandler;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
 
-@RequiredArgsConstructor
+@JsonDeserialize(using = PasswordDeserializer.class)
+@JsonSerialize(using = PasswordSerializer.class)
 public class Password {
     @PasswordConstraint
     @JsonProperty
     private final String rawPassword;
+
+    public Password(String rawPassword) {
+        this.rawPassword = rawPassword;
+    }
 
     public boolean isMatch(final String encryptedPassword, final PasswordHandler passwordHandler) {
         return passwordHandler.matches(rawPassword, encryptedPassword);
@@ -22,5 +38,24 @@ public class Password {
     @Override
     public String toString() {
         return "XXXX";
+    }
+
+    static class PasswordDeserializer extends JsonDeserializer<Password> {
+        @Override
+        public Password deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                throws IOException, JacksonException {
+            // JSON에서 비밀번호 값 읽기
+            String rawPassword = jsonParser.getText().trim();
+            // Password 객체 생성하여 반환
+            return new Password(rawPassword);
+        }
+    }
+
+    static class PasswordSerializer extends JsonSerializer<Password> {
+        @Override
+        public void serialize(Password password, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+                throws IOException {
+            jsonGenerator.writeString(password.rawPassword);
+        }
     }
 }
