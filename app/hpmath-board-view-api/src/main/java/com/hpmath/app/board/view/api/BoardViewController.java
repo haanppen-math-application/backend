f9f7478e8f359ce5b748ae99add62f9442cf6660
huhdy32 <mong3268@gmail.com>
2025-05-15 18:41:42 +0900

    fix(board-view-api): uri 설정

--- app/hpmath-board-view-api/src/main/java/com/hpmath/app/board/view/api/BoardViewController.java
+++ app/hpmath-board-view-api/src/main/java/com/hpmath/app/board/view/api/BoardViewController.java
@@ -6,16 +6,16 @@ import com.hpmath.common.web.authenticationV2.Authorization;
 import com.hpmath.common.web.authenticationV2.LoginInfo;
 import com.hpmath.domain.board.view.ViewCountService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
+@RequestMapping("/api/v1/board/view-count")
 public class BoardViewController {
     private final ViewCountService viewCountService;
 
