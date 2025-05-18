01887ecc785bde32d5b1aff4394b76c6eb8a3fd0
huhdy32 <mong3268@gmail.com>
2025-05-19 00:42:39 +0900

    refactor: 사용되지 않는 쿼리 삭제

--- domain/hpmath-course/src/main/java/com/hpmath/domain/course/repository/MemoMediaRepository.java
+++ domain/hpmath-course/src/main/java/com/hpmath/domain/course/repository/MemoMediaRepository.java
@@ -1,20 +1,13 @@
 package com.hpmath.domain.course.repository;
 
 import com.hpmath.domain.course.entity.MemoMedia;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemoMediaRepository extends JpaRepository<MemoMedia, Long> {
-    void deleteMemoMediaByMemo_Id(final Long memoId);
-    List<MemoMedia> findAllByMemo_Id(final Long memoId);
-    Optional<MemoMedia> findMemoMediaByIdAndMemo_Id(final Long memoMediaId, final Long memoId);
-
     @Query("SELECT mm from MemoMedia mm "
             + "JOIN FETCH mm.memo "
             + "JOIN FETCH mm.memoMediaAttachments "
