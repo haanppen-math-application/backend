02b77eb068099799a1f9296684246e7907773cc7
huhdy32 <mong3268@gmail.com>
2025-05-19 10:56:55 +0900

    refactor(directory): 레포지토리 직접 호출 하도록 변경

--- app/consumer/hpmath-directory/src/main/java/com/hpmath/app/consumer/directory/handler/MemberDeleteDirectoryEventHandler.java
+++ app/consumer/hpmath-directory/src/main/java/com/hpmath/app/consumer/directory/handler/MemberDeleteDirectoryEventHandler.java
@@ -3,19 +3,22 @@ package com.hpmath.app.consumer.directory.handler;
 import com.hpmath.common.event.Event;
 import com.hpmath.common.event.EventType;
 import com.hpmath.common.event.payload.MemberDeletedEventPayload;
-import com.hpmath.domain.directory.service.delete.DirectoryDeleteService;
+import com.hpmath.domain.directory.dao.DirectoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class MemberDeleteDirectoryEventHandler implements EventHandler<MemberDeletedEventPayload> {
-    private final DirectoryDeleteService directoryDeleteService;
+    private final DirectoryRepository directoryRepository;
 
     @Override
+    @Transactional
     public void handle(Event<MemberDeletedEventPayload> event) {
         final MemberDeletedEventPayload payload = event.getPayload();
-        directoryDeleteService.deleteOwnerInformation(payload.memberIds());
+
+        directoryRepository.updateOwnerInfoToNullIdsIn(payload.memberIds());
     }
 
     @Override
--- domain/hpmath-directory/src/main/java/com/hpmath/domain/directory/service/delete/DirectoryDeleteService.java
+++ domain/hpmath-directory/src/main/java/com/hpmath/domain/directory/service/delete/DirectoryDeleteService.java
@@ -1,9 +1,7 @@
 package com.hpmath.domain.directory.service.delete;
 
-import com.hpmath.domain.directory.dao.DirectoryRepository;
 import com.hpmath.domain.directory.dto.DeleteDirectoryCommand;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,16 +13,10 @@ import org.springframework.validation.annotation.Validated;
 public class DirectoryDeleteService {
     private final DirectoryDeleteCommandCreator directoryDeleteCommandCreator;
     private final DirectoryDeleteManager directoryDeleteManager;
-    private final DirectoryRepository directoryRepository;
 
     @Transactional
     public void delete(@Valid final DeleteDirectoryCommand deleteDirectoryDto) {
         final DirectoryDeleteCommand directoryDeleteCommand = directoryDeleteCommandCreator.create(deleteDirectoryDto);
         directoryDeleteManager.delete(directoryDeleteCommand);
-    }
-
-    @Transactional
-    public void deleteOwnerInformation(final List<Long> memberIds) {
-        directoryRepository.updateOwnerInfoToNullIdsIn(memberIds);
     }
 }
