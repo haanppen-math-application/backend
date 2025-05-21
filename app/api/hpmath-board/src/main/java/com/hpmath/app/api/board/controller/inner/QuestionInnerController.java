e77d0e0d3fb814972768c6666ad02944ba13ebfa
huhdy32 <mong3268@gmail.com>
2025-05-21 10:53:32 +0900

    feat(board-question): 질문 조회 api 추가

---
+++ app/api/hpmath-board/src/main/java/com/hpmath/app/api/board/controller/inner/QuestionInnerController.java
@@ -0,0 +1,60 @@
+package com.hpmath.app.api.board.controller.inner;
+
+import com.hpmath.domain.board.dto.QuestionInfoResult;
+import com.hpmath.domain.board.service.question.QuestionQueryService;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class QuestionInnerController {
+    private final QuestionQueryService questionQueryService;
+
+    @GetMapping("/api/inner/v1/questions/paged/date-desc")
+    public ResponseEntity<QuestionInfoResponses> getPagedQuestions(
+            @RequestParam Long pageSize,
+            @RequestParam Long offset
+    ) {
+        final List<QuestionInfoResponse> responses = questionQueryService.loadQuestionsSortByDate(pageSize, offset).stream()
+                .map(QuestionInfoResponse::from)
+                .toList();
+        return ResponseEntity.ok(QuestionInfoResponses.from(responses));
+    }
+
+    record QuestionInfoResponses(
+            List<QuestionInfoResponse> questions
+    ) {
+        static QuestionInfoResponses from(List<QuestionInfoResponse> responses) {
+            return new QuestionInfoResponses(responses);
+        }
+    }
+
+    record QuestionInfoResponse(
+            Long questionId,
+            String title,
+            String content,
+            Boolean solved,
+            LocalDateTime registeredDateTime,
+            Long ownerId,
+            Long targetId,
+            List<String> mediaSrcs
+    ) {
+        public static QuestionInfoResponse from(final QuestionInfoResult questionInfoResult) {
+            return new QuestionInfoResponse(
+                    questionInfoResult.questionId(),
+                    questionInfoResult.title(),
+                    questionInfoResult.content(),
+                    questionInfoResult.solved(),
+                    questionInfoResult.registeredDateTime(),
+                    questionInfoResult.ownerId(),
+                    questionInfoResult.targetId(),
+                    questionInfoResult.mediaSrcs()
+            );
+        }
+    }
+}
---
+++ client/hpmath-board-question-client/src/main/java/com/hpmath/client/board/question/BoardQuestionClient.java
@@ -0,0 +1,68 @@
+package com.hpmath.client.board.question;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class BoardQuestionClient {
+    private final RestClient restClient;
+
+    public BoardQuestionClient(
+            @Value("${client.board-view.url:http://localhost:80}") final String baseUrl
+    ) {
+        this.restClient = RestClient.builder()
+                .baseUrl(baseUrl)
+                .requestFactory(timeoutConfigurationFactory())
+                .build();
+    }
+
+    private static ClientHttpRequestFactory timeoutConfigurationFactory() {
+        final SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(Duration.ofSeconds(1));
+        clientHttpRequestFactory.setReadTimeout(Duration.ofSeconds(1));
+        return clientHttpRequestFactory;
+    }
+
+    public List<QuestionDetailInfo> getQuestionsSortByDate(final int offset, final int limit) {
+        final QuestionDetailInfos infos = restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/api/inner/v1/questions/paged/date-desc")
+                        .queryParam("sort", "date")
+                        .queryParam("pageSize", limit)
+                        .queryParam("offset", offset)
+                        .build())
+                .retrieve()
+                .body(QuestionDetailInfos.class);
+        return infos.questions() == null ? null : infos.questions();
+    }
+
+    public QuestionDetailInfo get(final Long questionId) {
+        return restClient.get().uri(uriBuilder -> uriBuilder.path("test")
+                        .queryParam("questionId", questionId)
+                        .build())
+                .retrieve()
+                .body(QuestionDetailInfo.class);
+    }
+
+    private record QuestionDetailInfos(
+            List<QuestionDetailInfo> questions
+    ){
+    }
+
+    public record QuestionDetailInfo(
+            Long questionId,
+            String title,
+            String content,
+            Boolean solved,
+            LocalDateTime registeredDateTime,
+            Long ownerId,
+            Long targetId,
+            List<String> mediaSrcs
+    ) {
+    }
+}
