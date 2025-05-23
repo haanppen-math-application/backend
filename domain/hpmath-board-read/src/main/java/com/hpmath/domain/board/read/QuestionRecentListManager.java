5105c54c240f95dfffe063c27ebe8baa60235373
huhdy32 <mong3268@gmail.com>
2025-05-23 17:41:54 +0900

    feat: 최근 글 목록 초기화 기능 추가

--- domain/hpmath-board-read/src/main/java/com/hpmath/domain/board/read/QuestionRecentListManager.java
+++ domain/hpmath-board-read/src/main/java/com/hpmath/domain/board/read/QuestionRecentListManager.java
@@ -6,9 +6,13 @@ import com.hpmath.domain.board.read.repository.RecentQuestionRepository;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 class QuestionRecentListManager {
@@ -19,13 +23,26 @@ class QuestionRecentListManager {
 
     @Async("workers")
     public CompletableFuture<List<Long>> getPagedResultSortedByDate(final int pageNumber, final int pageSize) {
+        final List<Long> questionIds = getRecentQuestionIds(pageNumber, pageSize);
+        return CompletableFuture.completedFuture(questionIds);
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    void initialize() {
+        log.info("Initializing recent question list manager");
+        final List<Long> questionCount = getRecentQuestionIds(0, RECENT_QUESTION_CACHE_SIZE.intValue());
+        log.info("Initializing recent questions count: {}", questionCount);
+    }
+
+    private List<Long> getRecentQuestionIds(final int pageNumber, final int pageSize) {
         List<Long> questionIds = recentQuestionRepository.getRange(pageNumber * pageSize, pageSize);
         if (questionIds.size() != pageSize) {
             questionIds = boardQuestionClient.getQuestionsSortByDate(pageNumber, pageSize).stream()
-                    .peek(question -> recentQuestionRepository.add(question.questionId(), question.registeredDateTime(), RECENT_QUESTION_CACHE_SIZE))
+                    .peek(question -> recentQuestionRepository.add(question.questionId(), question.registeredDateTime(),
+                            RECENT_QUESTION_CACHE_SIZE))
                     .map(QuestionDetailInfo::questionId)
                     .toList();
         }
-        return CompletableFuture.completedFuture(questionIds);
+        return questionIds;
     }
 }
