039bff57ab4e07790fc74f5e2a4d4998d1b91872
huhdy32 <mong3268@gmail.com>
2025-05-23 12:44:24 +0900

    feat: request collaps 뷰 조회에 추가

--- domain/hpmath-board-read/src/main/java/com/hpmath/domain/board/read/BoardViewManager.java
+++ domain/hpmath-board-read/src/main/java/com/hpmath/domain/board/read/BoardViewManager.java
@@ -1,6 +1,7 @@
 package com.hpmath.domain.board.read;
 
 import com.hpmath.client.board.view.BoardViewClient;
+import com.hpmath.common.collapse.cache.CollapseCache;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class BoardViewManager {
     private final BoardViewClient boardViewClient;
 
+    @CollapseCache(keyPrefix = "board-view::question::view::count", logicalTTLSeconds = 5)
     public Long getViewCount(final Long questionId) {
         log.info("Getting view count for question: {}", questionId);
         return boardViewClient.getViewCount(questionId);
